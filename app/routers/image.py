996151dc4d639c45df0dbb9dff231a4f5cc4f12f
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-10 19:31:35 +0100

    edit - use UploadRequestBody at endpoint POST /image-api/upload

--- app/routers/image.py
+++ app/routers/image.py
@@ -1,11 +1,13 @@
 
 from fastapi import APIRouter
+from app.models.image_api.upload import UploadRequestBody
 
 router = APIRouter()
 
 
 @router.post("/upload/")
-async def upload():
+async def upload(body: UploadRequestBody):
     # TODO: this is a POST request which will allow a user to upload an image
     print('hello from upload')
-    return {"filename": 'file.filename', "message": "Image processed successfully.", "your_choice": "your choice was " + "user_choice" + "."}
+    print(body)
+    return {"filename": 'file.filename', "message": "Image processed successfully.", "your_choice": "your choice was " + body.processing + "."}
