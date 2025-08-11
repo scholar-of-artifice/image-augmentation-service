45957a50dae7aea3d82ab1264d0f7713138eb087
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-11 18:44:12 +0100

    edit - simplify response of images-api/upload

--- app/routers/image.py
+++ app/routers/image.py
@@ -10,4 +10,4 @@ async def upload(body: UploadRequestBody):
     # TODO: this is a POST request which will allow a user to upload an image
     print('hello from upload')
     print(body)
-    return {"filename": 'file.filename', "message": "Image processed successfully.", "your_choice": "your choice was " + body.processing + "."}
+    return {"filename": 'file.filename', "message": "Image processed successfully."}
