8919b05eb21d6827cfa611a30658f2f4af5050cd
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-11 18:47:33 +0100

    edit - create a submodel for arguments to shift function

--- app/models/image_api/upload.py
+++ app/models/image_api/upload.py
@@ -1,5 +1,7 @@
 from enum import Enum
-from pydantic import BaseModel
+from typing import Annotated
+from pydantic import BaseModel, Field
+from pydantic.types import StringConstraints
 
 # TODO: there will be more functions listed here
 # TODO: handle parameters for each function
@@ -10,9 +12,23 @@ class ProcessingEnum(str, Enum):
     rotate = "rotate"
 
 
+class ShiftArguments(BaseModel):
+    # enforce specific string constraints
+    direction: Annotated[str, StringConstraints(
+        min_length=2,
+        max_length=5,
+        strip_whitespace=True,
+        strict=True,
+        to_lower=True,
+        pattern="^(up|down|left|right)$")]
+    # enforce positive integer... 0 is no change
+    distance: Annotated[int, Field(strict=True, gt=0)]
+
+
 class UploadRequestBody(BaseModel):
     """
     This is the request body for:
         /image-api/upload
     """
     processing: ProcessingEnum
+    arguments: ShiftArguments
