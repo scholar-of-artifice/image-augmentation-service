689249dd3d4442edeffc27249632921737f431b7
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-12 00:04:35 +0100

    edit - model validation on UploadRequestBody

--- app/models/image_api/upload.py
+++ app/models/image_api/upload.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from typing import Annotated, Union
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator, ValidationError
 from pydantic.types import StringConstraints
 
 # TODO: there will be more functions listed here
@@ -37,3 +37,23 @@ class UploadRequestBody(BaseModel):
     """
     processing: ProcessingEnum
     arguments: Union[ShiftArguments, RotateArguments]
+
+    @model_validator(mode='before')
+    def validate_arguments_match_processing(cls, data: dict) -> dict:
+        # TODO: Comment and docstring
+        processing, arguments = data.get("processing"), data.get("arguments")
+        if not processing or not arguments:
+            raise ValueError(
+                "Both processing and arguments fields are required.")
+        if processing == ProcessingEnum.shift:
+            try:
+                ShiftArguments.model_validate(arguments)
+            except ValidationError as e:
+                raise ValueError(f"Arguments for shift are invalid. {e}")
+        if processing == ProcessingEnum.rotate:
+            try:
+                RotateArguments.model_validate(arguments)
+            except ValidationError as e:
+                raise ValueError(f"Arguments for rotate are invalid: {e}")
+
+        return data
