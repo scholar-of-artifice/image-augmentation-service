577d9731170d244d6533d428e94eaa6709a0ed88
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-12 18:59:58 +0100

    edit - expand upload api to take files and json arguments as a multipart upload using a form

--- app/routers/image.py
+++ app/routers/image.py
@@ -1,13 +1,14 @@
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Form, UploadFile
 from app.models.image_api.upload import UploadRequestBody
 
 router = APIRouter()
 
 
 @router.post("/upload/")
-async def upload(body: UploadRequestBody):
-    # TODO: this is a POST request which will allow a user to upload an image
+async def upload(body: str = Form(...), file: UploadFile = Form(...)):
     print('hello from upload')
+    # TODO: validate form data within this request
     print(body)
+    print(file.filename)
     return {"filename": 'file.filename', "message": "Image processed successfully."}
