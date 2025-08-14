238d370d9c5a308bddd9fdd6379686ecdbddc132
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-14 23:01:22 +0100

    fix - failed test

--- tests/app/models/image_api/test_upload.py
+++ tests/app/models/image_api/test_upload.py
@@ -1,54 +1,65 @@
 import pytest
 from pydantic import ValidationError
-from app.models.image_api.upload import ProcessingEnum, ShiftArguments, RotateArguments, UploadRequestBody
-
-
-def test_ProcessingEnum_has_shift():
-    assert ProcessingEnum.shift == "shift"
-
-
-def test_ProcessingEnum_has_rotate():
-    assert ProcessingEnum.rotate == "rotate"
+from app.models.image_api.upload import ShiftArguments, RotateArguments, UploadRequestBody
 
 
 def test_ShiftArguments_up_is_a_valid_direction():
     data = {
+        "processing": "shift",
         "direction": "up",
         "distance": 42,
     }
+    assert ShiftArguments(**data).processing == "shift"
     assert ShiftArguments(**data).direction == "up"
     assert ShiftArguments(**data).distance == 42
 
 
 def test_ShiftArguments_down_is_a_valid_direction():
     data = {
+        "processing": "shift",
         "direction": "down",
         "distance": 42,
     }
+    assert ShiftArguments(**data).processing == "shift"
     assert ShiftArguments(**data).direction == "down"
     assert ShiftArguments(**data).distance == 42
 
 
 def test_ShiftArguments_left_is_a_valid_direction():
     data = {
+        "processing": "shift",
         "direction": "left",
         "distance": 42,
     }
+    assert ShiftArguments(**data).processing == "shift"
     assert ShiftArguments(**data).direction == "left"
     assert ShiftArguments(**data).distance == 42
 
 
 def test_ShiftArguments_right_is_a_valid_direction():
     data = {
+        "processing": "shift",
         "direction": "right",
         "distance": 42,
     }
+    assert ShiftArguments(**data).processing == "shift"
     assert ShiftArguments(**data).direction == "right"
     assert ShiftArguments(**data).distance == 42
 
 
+def test_ShiftArguments_rotate_is_an_invalid_processing_type():
+    data = {
+        "processing": "rotate",
+        "direction": "right",
+        "distance": 42,
+    }
+    with pytest.raises(ValidationError):
+        ShiftArguments(**data)
+
+
 def test_ShiftArguments_asdf_is_an_invalid_direction():
     data = {
+        "processing": "shift",
         "direction": "asdf",
         "distance": 42,
     }
@@ -58,6 +69,7 @@ def test_ShiftArguments_asdf_is_an_invalid_direction():
 
 def test_ShiftArguments_diagonal_is_an_invalid_direction():
     data = {
+        "processing": "shift",
         "direction": "diagonal",
         "distance": 42,
     }
@@ -67,6 +79,7 @@ def test_ShiftArguments_diagonal_is_an_invalid_direction():
 
 def test_ShiftArguments_blank_string_is_an_invalid_direction():
     data = {
+        "processing": "shift",
         "direction": "",
         "distance": 42,
     }
@@ -76,6 +89,7 @@ def test_ShiftArguments_blank_string_is_an_invalid_direction():
 
 def test_ShiftArguments_None_is_an_invalid_direction():
     data = {
+        "processing": "shift",
         "direction": None,
         "distance": 42,
     }
@@ -85,6 +99,7 @@ def test_ShiftArguments_None_is_an_invalid_direction():
 
 def test_ShiftArguments_0_is_an_invalid_distance():
     data = {
+        "processing": "shift",
         "direction": "up",
         "distance": 0,
     }
@@ -94,6 +109,7 @@ def test_ShiftArguments_0_is_an_invalid_distance():
 
 def test_ShiftArguments_negative_value_is_an_invalid_distance():
     data = {
+        "processing": "shift",
         "direction": "up",
         "distance": -123,
     }
@@ -105,13 +121,24 @@ def test_RotateArguments_values_between_1_and_359_are_valid_amount():
     valid_amount = list(range(1, 360))
     for v in valid_amount:
         data = {
+            "processing": "rotate",
             "amount": v
         }
         assert RotateArguments(**data).amount == v
 
 
+def test_RotateArguments_shift_is_an_invalid_processing_type():
+    data = {
+        "processing": "shift",
+        "amount": 42
+    }
+    with pytest.raises(ValidationError):
+        RotateArguments(**data)
+
+
 def test_RotateArguments_negative_number_is_invalid_amount():
     data = {
+        "processing": "rotate",
         "amount": -42
     }
     with pytest.raises(ValidationError):
@@ -120,6 +147,7 @@ def test_RotateArguments_negative_number_is_invalid_amount():
 
 def test_RotateArguments_0_is_invalid_amount():
     data = {
+        "processing": "rotate",
         "amount": 0
     }
     with pytest.raises(ValidationError):
@@ -128,48 +156,8 @@ def test_RotateArguments_0_is_invalid_amount():
 
 def test_RotateArguments_360_is_invalid_amount():
     data = {
+        "processing": "rotate",
         "amount": 360
     }
     with pytest.raises(ValidationError):
         RotateArguments(**data)
-
-
-def test_UploadRequestBody_shift_with_ShiftArguments_is_valid():
-    # TODO: comment this better
-    data = {
-        "processing": ProcessingEnum.shift,
-        "arguments": ShiftArguments(direction="up", distance=10)
-    }
-    assert UploadRequestBody(**data).processing == "shift"
-    assert UploadRequestBody(**data).arguments.direction == "up"
-    assert UploadRequestBody(**data).arguments.distance == 10
-
-
-def test_UploadRequestBody_rotate_with_RotateArguments_is_valid():
-    # TODO: comment this better
-    data = {
-        "processing": ProcessingEnum.rotate,
-        "arguments": RotateArguments(amount=10)
-    }
-    assert UploadRequestBody(**data).processing == "rotate"
-    assert UploadRequestBody(**data).arguments.amount == 10
-
-
-def test_UploadRequestBody_shift_with_RotateArguments_raise_ValidationError():
-    # TODO: comment this better
-    data = {
-        "processing": ProcessingEnum.shift,
-        "arguments": RotateArguments(amount=10)
-    }
-    with pytest.raises(ValidationError):
-        UploadRequestBody(**data)
-
-
-def test_UploadRequestBody_rotate_with_ShiftArguments_raise_ValidationError():
-    # TODO: comment this better
-    data = {
-        "processing": ProcessingEnum.rotate,
-        "arguments": ShiftArguments(direction="up", distance=10)
-    }
-    with pytest.raises(ValidationError):
-        UploadRequestBody(**data)
