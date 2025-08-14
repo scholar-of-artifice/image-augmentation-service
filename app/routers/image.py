b9cb4a3d42ecf0514c86b0689240db25f8b1f9d6
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-14 23:16:49 +0100

    edit - change /upload endpoint to validate input json using pydantic models

--- app/routers/image.py
+++ app/routers/image.py
@@ -1,6 +1,8 @@
-
+import json
 from fastapi import APIRouter, Form, UploadFile
 from app.models.image_api.upload import UploadRequestBody
+from pydantic import ValidationError
+
 
 router = APIRouter()
 
@@ -8,7 +10,28 @@ router = APIRouter()
 @router.post("/upload/")
 async def upload(body: str = Form(...), file: UploadFile = Form(...)):
     print('hello from upload')
-    # TODO: validate form data within this request
     print(body)
     print(file.filename)
-    return {"filename": 'file.filename', "message": "Image processed successfully."}
+    # TODO: validate form data within this request
+    try:
+        json_data = json.loads(body)
+        validated_data = UploadRequestBody(**json_data)
+        arguments = validated_data.arguments
+        if arguments.processing == "shift":
+            # TODO: apply shift to image
+            print("shift")
+        elif arguments.processing == "rotate":
+            # TODO: apply rotate to image
+            print("rotate")
+        return {
+            "filename": file.filename,
+            "message": "Image processed successfully."
+        }
+    except json.JSONDecodeError:
+        return {"error": "Invalid JSON format in the 'body' field"}
+    except ValidationError as e:
+        return {"error": str(e)}
+    return {
+        "filename": file.filename,
+        "message": "Image not processed."
+    }
