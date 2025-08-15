1cf00f068477317c0761fc8c27f91769eab08f9c
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-15 22:04:02 +0100

    add - write_numpy_array_to_image_file

--- app/internal/file_handling.py
+++ app/internal/file_handling.py
@@ -23,3 +23,16 @@ def translate_file_to_numpy_array(content: bytes) -> numpy.ndarray:
             return numpy.array(img)
     except Exception as e:
         raise InvalidImageFileError(f"failed to open or convert image {e}")
+
+
+def write_numpy_array_to_image_file(data: numpy.ndarray, file_name: str) -> str:
+    """
+        TODO: create docstring
+    """
+    # TODO: comment this code
+    img_data = Image.fromarray(data)
+    # TODO: change write location
+    file_path = 'app/_tmp/' + file_name + '.png'
+    img_data.save('app/_tmp/' + file_name + '.png', 'PNG')
+    # TODO: change what is returned
+    return file_path
