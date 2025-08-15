150f9ab8e39d19dd1ab0cbfc8c5eae8a1e51eebe
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-15 20:39:13 +0100

    edit - add test test_ShiftArguments_string_number_is_an_invalid_distance

--- tests/app/models/image_api/test_upload.py
+++ tests/app/models/image_api/test_upload.py
@@ -121,6 +121,16 @@ def test_ShiftArguments_None_is_an_invalid_distance():
         ShiftArguments(**data)
 
 
+def test_ShiftArguments_string_number_is_an_invalid_distance():
+    data = {
+        "processing": "shift",
+        "direction": "up",
+        "distance": "42",
+    }
+    with pytest.raises(ValidationError):
+        ShiftArguments(**data)
+
+
 def test_ShiftArguments_0_is_an_invalid_distance():
     data = {
         "processing": "shift",
