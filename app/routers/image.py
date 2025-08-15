f294517468b795baa3aa8b8ef84c140b4c1734af
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-15 22:06:53 +0100

    edit - upload implementation for multipart upload and processing of file

--- app/routers/image.py
+++ app/routers/image.py
@@ -1,37 +1,48 @@
 import json
-from fastapi import APIRouter, Form, UploadFile
+from fastapi import APIRouter, Form, File, UploadFile
 from app.models.image_api.upload import UploadRequestBody
 from pydantic import ValidationError
+from app.internal.augmentations import shift, rotate
+from app.internal.file_handling import translate_file_to_numpy_array, write_numpy_array_to_image_file
 
 
 router = APIRouter()
 
 
 @router.post("/upload/")
-async def upload(body: str = Form(...), file: UploadFile = Form(...)):
+async def upload(file: UploadFile, body: str = Form(...)):
     print('hello from upload')
-    print(body)
-    print(file.filename)
-    # TODO: validate form data within this request
     try:
+        # get the json data
         json_data = json.loads(body)
         validated_data = UploadRequestBody(**json_data)
-        arguments = validated_data.arguments
-        if arguments.processing == "shift":
-            # TODO: apply shift to image
-            print("shift")
-        elif arguments.processing == "rotate":
-            # TODO: apply rotate to image
-            print("rotate")
+        # get image contents
+        image_content = await file.read()
+        img_data = translate_file_to_numpy_array(image_content)
+        # process image based on requested algorithm
+        new_img_data = img_data
+        file_path = ''
+        if validated_data.arguments.processing == "shift":
+            new_img_data = shift(image_data=img_data,
+                                 direction=validated_data.arguments.direction,
+                                 distance=validated_data.arguments.distance)
+        elif validated_data.arguments.processing == "rotate":
+            new_img_data = rotate(image_data=img_data,
+                                  amount=validated_data.arguments.amount)
+        file_path = write_numpy_array_to_image_file(data=new_img_data,
+                                                    file_name='i_hope_this_works')
+        # tell me where you put the file?
         return {
-            "filename": file.filename,
-            "message": "Image processed successfully."
+            "output_file_path": file_path,
+            "body": body
         }
     except json.JSONDecodeError:
+        # this should only happen when json.loads has an issue
         return {"error": "Invalid JSON format in the 'body' field"}
     except ValidationError as e:
+        # this should happen when any validation fails
         return {"error": str(e)}
     return {
-        "filename": file.filename,
-        "message": "Image not processed."
+        "input_filename": file.filename,
+        "body": body
     }
