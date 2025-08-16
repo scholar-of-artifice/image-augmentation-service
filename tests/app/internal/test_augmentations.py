f3a17b84f77b645a45872e8cd17ea028a563469c
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-16 08:30:22 +0100

    edit - add test test_shift_blank_string_direction_raises_exception

--- tests/app/internal/test_augmentations.py
+++ tests/app/internal/test_augmentations.py
@@ -115,6 +115,22 @@ def test_shift_None_direction_raises_exception():
         shift(input_image, None, 3)
 
 
+def test_shift_blank_string_direction_raises_exception():
+    """
+    GIVEN a 4x4 matrix
+    AND the direction is a blank string
+    AND the distance is 3
+    WHEN shift is called
+    THEN it raises a ValueError
+    """
+    input_image = numpy.array([[1, 0, 0, 0],
+                               [0, 2, 0, 0],
+                               [0, 0, 3, 0],
+                               [0, 0, 0, 4]])
+    with pytest.raises(ValueError):
+        shift(input_image, '', 3)
+
+
 def test_rotate_example_45_degrees():
     """
     GIVEN a 4x4 matrix
