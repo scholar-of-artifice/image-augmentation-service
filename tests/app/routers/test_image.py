db1b87467ca81d9861a8a2e6d9683bf311038e9a
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-17 08:57:55 +0100

    edit - add test test_upload_is_successful_when_request_is_valid_shift

--- tests/app/routers/test_image.py
+++ tests/app/routers/test_image.py
@@ -1,5 +1,6 @@
 import json
 from fastapi import FastAPI
+from pathlib import Path
 from fastapi.testclient import TestClient
 from app.routers.image import router
 
@@ -8,23 +9,27 @@ app.include_router(router)
 
 client = TestClient(app)
 
+TEST_DIR = Path(__file__).parent
+TEST_IMAGES_PATH = TEST_DIR.parent.parent / "data" / "basic_shapes_250x250.png"
 
-def test_upload_is_successful_when_request_is_valid():
-    # TODO: make request body
-    json_data = {
+def test_upload_is_successful_when_request_is_valid_shift():
+    """
+    GIVEN a valid image_file
+    WHEN .../upload is called
+    THEN the request is successful
+    """
+    input_request_body = {
         "arguments": {
-            "processing": "rotate",
-            "amount": 10
+            "processing": "shift",
+            "direction": "left",
+            "distance": 50
         }
     }
-    dummy_files = {
-        "file": ("test_image.jpg", b"dummy image content", "image/jpeg")
-    }
-    response = client.post(
-        url="/upload/",
-        data={"body": json.dumps(json_data)},
-        files=dummy_files
-    )
-    assert response.status_code == 200
-    assert response.json() == {"filename": "test_image.jpg",
-                               "message": "Image processed successfully."}
+    with open(file=TEST_IMAGES_PATH, mode="rb") as image_file:
+        response = client.post("/upload",
+                               data={"body": json.dumps(input_request_body)},
+                               files={"file": ("basic_shapes_250x250.png", image_file, "image/png")}
+                               )
+        response_json = response.json()
+        assert(response.status_code == 200)
+        assert(response_json["output_file_path"] == 'app/_tmp/i_hope_this_works.png')
