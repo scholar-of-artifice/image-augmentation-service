d5f422cb6c9feeb688b4d5f96ff89fe5775a64fd
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-17 18:33:18 +0100

    edit - add test test_upload_is_not_successful_when_request_is_invalid

--- tests/app/routers/test_image.py
+++ tests/app/routers/test_image.py
@@ -1,5 +1,5 @@
 import json
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from pathlib import Path
 from fastapi.testclient import TestClient
 from app.routers.image import router
@@ -32,5 +32,30 @@ def test_upload_is_successful_when_request_is_valid():
                                )
         response_json = response.json()
         assert(response.status_code == 200)
+        # TODO: should i assert this?
+        #  assert(response_json["output_file_path"] == 'app/_tmp/[some_file_name].png')
+
+
+def test_upload_is_not_successful_when_request_is_invalid():
+    """
+    GIVEN a valid image_file
+    AND a bad request
+    WHEN .../upload is called
+    THEN the request is not successful
+    """
+    input_request_body = {
+        "arguments": {
+            "processing": "rotate",
+            "direction": "left",
+            "distance": 50
+        }
+    }
+    with open(file=TEST_IMAGES_PATH, mode="rb") as image_file:
+        response = client.post("/upload",
+                               data={"body": json.dumps(input_request_body)},
+                               files={"file": ("basic_shapes_250x250.png", image_file, "image/png")}
+                               )
+        response_json = response.json()
+        assert(response.status_code == status.HTTP_400_BAD_REQUEST)
         # TODO: should i assert this?
         #  assert(response_json["output_file_path"] == 'app/_tmp/[some_file_name].png')
