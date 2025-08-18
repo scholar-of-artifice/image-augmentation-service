fb872dabfbd23d64ecd238faff1a50f76fcc3558
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-18 18:59:12 +0100

    edit - add test test_upload_is_not_successful_when_request_uses_invalid_model

--- tests/app/routers/test_image.py
+++ tests/app/routers/test_image.py
@@ -53,6 +53,29 @@ def test_upload_is_not_successful_when_request_uses_invalid_JSON():
                                )
         assert(response.status_code == status.HTTP_400_BAD_REQUEST)
 
+def test_upload_is_not_successful_when_request_uses_invalid_model():
+    """
+    GIVEN a valid image_file
+    AND a bad request with invalid JSON
+    WHEN .../upload is called
+    THEN the request is not successful
+    """
+    input_request_body = {
+        "arguments": {
+            "processing": "rotate",
+            "direction": "left",
+            "distance": 50
+        }
+    }
+    input_request_str = json.dumps(input_request_body)
+    input_request_str = input_request_str[:-5]
+    with open(file=TEST_IMAGES_PATH, mode="rb") as image_file:
+        response = client.post("/upload",
+                               data={"body": input_request_str},
+                               files={"file": ("basic_shapes_250x250.png", image_file, "image/png")}
+                               )
+        assert(response.status_code == status.HTTP_400_BAD_REQUEST)
+
 
 def test_upload_is_not_successful_when_request_uses_invalid_request_model():
     """
