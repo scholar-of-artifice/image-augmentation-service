409cf9b193488fe2afcdc72f0e15d019ed5f3855
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-20 23:13:21 +0100

    edit - remove / endpoint

--- app/main.py
+++ app/main.py
@@ -5,9 +5,3 @@ app = FastAPI()
 app.include_router(image.router, prefix="/image-api")
 app.include_router(health.router, prefix="/healthcheck-api")
 
-
-@app.get("/")
-def root():
-    print("Hello from root")
-    message = "Hello from image-augmentation-service"
-    return {"message": message}
