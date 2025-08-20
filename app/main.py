f149e49750757edd6ec804b05c5b18d1c1a05912
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-20 23:12:35 +0100

    edit - add healthcheck endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from app.routers import image
+from app.routers import image, health
 
 app = FastAPI()
 app.include_router(image.router, prefix="/image-api")
+app.include_router(health.router, prefix="/healthcheck-api")
 
 
 @app.get("/")
