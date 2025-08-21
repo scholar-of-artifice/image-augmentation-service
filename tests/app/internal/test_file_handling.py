bc765f14d63e8863b6ba65b0d2735cef9e7581f5
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-21 22:51:04 +0100

    edit - improve create_dummy_image_bytes to give a slightly larger image with more real world values

--- tests/app/internal/test_file_handling.py
+++ tests/app/internal/test_file_handling.py
@@ -11,13 +11,17 @@ from app.internal.file_handling import translate_file_to_numpy_array, write_nump
 def create_dummy_image_bytes() -> bytes:
     """
         Helper function that creates a dummy image byte array for testing.
+
+        Returns:
+            bytes: dummy image byte array
     """
     # Create in memory byte buffer
     buffer = io.BytesIO()
-    # Create a simple 2x2 RGB image
+    # Create a simple 3x3 RGB image
     array = numpy.array( [
-        [[255, 0, 0], [0,255, 0]],
-        [[0, 0, 255], [255, 0, 0]]
+        [[255, 0, 0], [0,255, 0], [0, 0, 255]],
+        [[255, 255, 255], [127, 127, 127], [0, 0, 0]],
+        [[127, 0, 0], [0,127, 0], [0, 0, 127]],
     ], dtype=numpy.uint8)
     img = Image.fromarray(array)
     # save the image to the buffer in png format
