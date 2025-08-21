0c239105def5d051cd7e3c4c564258998c1e7a61
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-21 23:43:24 +0100

    edit - add new test test_healthcheck_has_correct_response_when_request_is_valid

--- tests/app/routers/test_health.py
+++ tests/app/routers/test_health.py
@@ -16,4 +16,13 @@ def test_healthcheck_is_successful_when_request_is_valid():
     """
     response = client.get("/healthcheck")
     assert response.status_code == status.HTTP_200_OK
+
+def test_healthcheck_has_correct_response_when_request_is_valid():
+    """
+        GIVEN a client
+        AND an endpoint of /healthcheck
+        WHEN a get request is made to the endpoint
+        THEN the correct response is returned.
+    """
+    response = client.get("/healthcheck")
     assert response.json() == {"status": "OK"}
