2d31428e2bfb818774a7051afc650fb5a6c957f4
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-22 18:00:12 +0100

    edit - add test app_includes_router_to_healthcheck_api_healthcheck_does_exist

--- tests/app/test_main.py
+++ tests/app/test_main.py
@@ -4,6 +4,15 @@ from app.main import app
 
 client = TestClient(app)
 
+def app_includes_router_to_healthcheck_api_healthcheck_does_exist():
+    """
+        GIVEN the app is up
+        WHEN /api/healthcheck-api/healthcheck/ is called
+        THEN the app should respond with 404
+    """
+    response = client.get("/api/healthcheck-api/healthcheck/")
+    assert response.status_code == status.HTTP_200_OK
+
 def app_includes_router_to_image_api_info_does_not_exist():
     """
         GIVEN the app is up
