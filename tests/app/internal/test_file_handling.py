9e1efd08499a0a50aea8b1399a2a53a6a168f137
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-22 17:57:07 +0100

    edit - add test test_write_numpy_array_to_image_file_to_invalid_file_path_raises_ValueError

--- tests/app/internal/test_file_handling.py
+++ tests/app/internal/test_file_handling.py
@@ -93,6 +93,25 @@ def test_write_numpy_array_to_image_file(tmp_path, monkeypatch):
     assert str(expected_file_path) == str(calculated_file_path)
     assert (expected_file_path).exists()
 
+def test_write_numpy_array_to_image_file_to_invalid_file_path_raises_ValueError(tmp_path, monkeypatch):
+    """
+        GIVEN a numpy array with valid image data
+        WHEN write_numpy_array_to_image_file is called
+        THEN an exception is raised.
+    """
+    # create a temporary directory for the 'unprocessed' image
+    unprocessed_dir = tmp_path / "unprocessed"
+    unprocessed_dir.mkdir()
+    # temporarily point the volume name to the test directory
+    monkeypatch.setitem(VOLUME_PATHS, "unprocessed_image_data", unprocessed_dir)
+    # execute the test
+    input_numpy_array = create_dummy_numpy_array()
+    file_name = 'test_image'
+    destination = 'this_destination_does_not_exist'
+    expected_file_path = (unprocessed_dir / f"{file_name}.png")
+    with pytest.raises(ValueError) as e:
+        write_numpy_array_to_image_file(data=input_numpy_array, file_name=file_name, destination_volume=destination )
+
 def test_create_file_name_returns_a_string():
     """
         GIVEN no arguments
