8ab78cb5d3eb06c5a2fdc0b6f35e4552a90d8c94
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-22 17:59:03 +0100

    edit - add test app_includes_router_to_image_api_info_does_not_exist

--- tests/app/test_main.py
+++ tests/app/test_main.py
@@ -4,3 +4,11 @@ from app.main import app
 
 client = TestClient(app)
 
+def app_includes_router_to_image_api_info_does_not_exist():
+    """
+        GIVEN the app is up
+        WHEN /api/image-api/info/ is called
+        THEN the app should respond with 404
+    """
+    response = client.get("/api/image-api/info/")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
