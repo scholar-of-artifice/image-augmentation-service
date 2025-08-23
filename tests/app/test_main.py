33e28e2b45d7e839cd4b86a7e97c794af0e295b7
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-23 12:18:56 +0100

    edit - imrpove docstring app_includes_router_to_image_api_info_does_not_exist

--- tests/app/test_main.py
+++ tests/app/test_main.py
@@ -15,7 +15,7 @@ def app_includes_router_to_healthcheck_api_healthcheck_does_exist():
 
 def app_includes_router_to_image_api_info_does_not_exist():
     """
-        GIVEN the app is up
+        GIVEN the app is running
         WHEN /api/image-api/info/ is called
         THEN the app should respond with 404
     """
