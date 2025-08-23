8c8417e61b375442238a04f929821eebad9e63da
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-23 10:23:51 +0100

    edit - read in logging configuration to main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from pathlib import Path
+import logging.config
+import json
 from app.routers import image, health
 
+def setup_logging():
+    config_file = Path(__file__).parent / "logging_config.json"
+    with config_file.open(mode='r') as f:
+        config = json.load(f)
+    logging.config.dictConfig(config)
+
+setup_logging()
 app = FastAPI()
 app.include_router(image.router, prefix="/image-api")
 app.include_router(health.router, prefix="/healthcheck-api")
