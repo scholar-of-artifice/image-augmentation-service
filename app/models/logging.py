3c1ce034b9f8fd6a35f8e5a9649fc8e2eec12503
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-23 20:35:16 +0100

    edit - add a date_time field for LogEntry

--- app/models/logging.py
+++ app/models/logging.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class LogEntry(BaseModel):
     """
         A basic LogEntry model.
     """
     # A short descriptive name for the event
+    date_time: datetime
     event: str
     details: str
