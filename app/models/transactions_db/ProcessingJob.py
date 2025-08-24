7fa0ec10c94b269b575df0cfdd79b21b1bf467a5
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-24 18:09:53 +0100

    add - ProcessingJob model for table, uses JobStatus enum for state of job

---
+++ app/models/transactions_db/ProcessingJob.py
@@ -0,0 +1,30 @@
+import enum
+from dataclasses import Field
+from sqlmodel import SQLModel
+from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql import JSONB
+from app.models.image_api.upload import UploadRequestBody
+
+class JobStatus(str, enum.Enum):
+    """
+        Enum for state of jobs.
+
+        SUCCEEDED AND FAILED are both valid end states.
+    """
+    PENDING = "pending"
+    PROCESSING = "processing"
+    SUCCEEDED = "succeeded"
+    FAILED = "failed"
+
+class ProcessingJob(SQLModel, table=True):
+    """
+        Class representing a processing job in the database.
+    """
+    # which processing job is this?
+    id: int = Field(primary_key=True)
+    # how is this processing jobs working out?
+    status: JobStatus = Field(JobStatus.PENDING)
+    # what was the user asking for?
+    request_parameters: UploadRequestBody = Field(sa_column=Column(JSONB))
+
+
