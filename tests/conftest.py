1890ffeeafc0417c2d6bbfe9587b71a3a581a7fa
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-25 19:17:21 +0100

    edit - create a PostgreSQL session for the tests to be run with

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+from sqlmodel import Session, SQLModel, create_engine
+from app.config import settings
+
+@pytest.fixture(scope="session")
+def engine():
+    """
+        Creates a SQLAlchemy engine instance
+    """
+    return create_engine(str(settings.DATABASE_URL))
+
+@pytest.fixture(scope="function")
+def db_session(engine):
+    """
+        Provides a SQLAlchemy session to the test database.
+        This fixture will be created once per test session.
+    """
+    # Create the tables if they don't exist
+    SQLModel.metadata.create_all(bind=engine)
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = Session(bind=engine)
+    try:
+        yield session
+    finally:
+        session.close()
+        transaction.rollback()
+        connection.close()
+    # Drop all tables after the test session is done
+    SQLModel.metadata.drop_all(bind=engine)
