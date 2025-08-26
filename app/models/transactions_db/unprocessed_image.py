20a0cb301823c24ff53c64ac50961f0d89fda775
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-26 21:38:21 +0100

    edit - remove event listener

--- app/models/transactions_db/unprocessed_image.py
+++ app/models/transactions_db/unprocessed_image.py
@@ -22,12 +22,3 @@ class UnprocessedImage(SQLModel, table=True):
         default_factory=lambda: datetime.now(timezone.utc),
         sa_column=Column(DateTime(timezone=True), nullable=False) # This tells SQLAlchemy to use a timezone-aware database column type
     )
-
-@event.listens_for(UnprocessedImage, "before_insert")
-def generate_storage_filepath(mapper, connection, target):
-    """
-        Generate the storage_filepath after the model is initialized.
-    """
-    # This check is useful if you ever create an instance with the path already provided
-    if target.storage_filepath is None:
-        target.storage_filepath = str(os.path.join(settings.UNPROCESSED_IMAGE_PATH, target.storage_filename))
