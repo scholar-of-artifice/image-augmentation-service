b777b8dc5a31f4e9d5e08e91f33fdf49993c577a
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-26 21:38:10 +0100

    edit - increase length of original_filename

--- app/models/transactions_db/unprocessed_image.py
+++ app/models/transactions_db/unprocessed_image.py
@@ -12,7 +12,7 @@ class UnprocessedImage(SQLModel, table=True):
     # who wrote this image?
     user_id: int = Field(foreign_key="users.id", nullable=False, index=True)
     # what is the original name of this image?
-    original_filename: str = Field(nullable=False, max_length=40)
+    original_filename: str = Field(nullable=False, max_length=255)
     # what is the uuid name of this image?
     storage_filename: str = Field(default_factory=lambda: f"{str(uuid.uuid4())}.png" , unique=True, nullable=False, max_length=40)
     # where is this image stored? This field will be populated by the logic in __post_init__
