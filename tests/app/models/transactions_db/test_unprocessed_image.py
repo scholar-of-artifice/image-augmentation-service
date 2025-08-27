0d5be3eec7df6ac6825382ccf9fe16670696e790
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-27 21:27:30 +0100

    edit - modify test  test_valid_unprocessed_image_saves_expected_data

--- tests/app/models/transactions_db/test_unprocessed_image.py
+++ tests/app/models/transactions_db/test_unprocessed_image.py
@@ -17,9 +17,14 @@ def test_valid_unprocessed_image_saves_expected_data(db_session: Session):
         AND it should have the correct storage_filepath
         AND it should have a created_at with the correct format
     """
+    user_to_create = User(external_id='some-1234-extr-0987-id45', name="Test User")
+    db_session.add(user_to_create)
+    db_session.commit()
+
     image_to_create = UnprocessedImage(
-        author_id= 101,
+        user_id= user_to_create.id,
         original_filename= "cool_image.png",
+        storage_filename="some_file_name.png"
     )
 
     db_session.add(image_to_create)
@@ -27,12 +32,9 @@ def test_valid_unprocessed_image_saves_expected_data(db_session: Session):
     db_session.refresh(image_to_create)
 
     assert image_to_create.id is not None
-    assert image_to_create.author_id == 101
+    assert image_to_create.user_id == user_to_create.id
     assert image_to_create.original_filename == 'cool_image.png'
-    assert image_to_create.storage_filename != 'cool_image.png'
-    assert image_to_create.storage_filepath == str(os.path.join(
-        settings.UNPROCESSED_IMAGE_PATH, image_to_create.storage_filename
-    ))
+    assert image_to_create.storage_filename == 'some_file_name.png'
     assert isinstance(image_to_create.created_at, datetime)
     assert image_to_create.created_at.tzinfo == timezone.utc
 
