01f7aa96b6dff90d146b50e99908917e32994114
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-27 21:20:17 +0100

    edit - make application name file and lengthen file name

--- app/models/transactions_db/processed_image.py
+++ app/models/transactions_db/processed_image.py
@@ -17,7 +17,7 @@ class ProcessedImage(SQLModel, table=True):
         nullable=False
     )
     # what is the uuid name of this image?
-    storage_filename: str = Field(default_factory=lambda: f"{str(uuid.uuid4())}.png" , unique=True, nullable=False, max_length=40)
+    storage_filename: str = Field(unique=True, nullable=False, max_length=255)
     # when was this image created?
     created_at: Optional[datetime] = Field(
         default_factory=lambda: datetime.now(timezone.utc),
