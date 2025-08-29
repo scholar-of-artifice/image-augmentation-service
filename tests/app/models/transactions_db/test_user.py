0b5ee1f629064cdd8a6350951340e82e8a3f3267
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-29 18:36:05 +0100

    edit - add test_user_IntegrityError_when_external_id_is_blank_string

--- tests/app/models/transactions_db/test_user.py
+++ tests/app/models/transactions_db/test_user.py
@@ -61,6 +61,20 @@ def test_user_IntegrityError_when_external_id_is_null(db_session: Session):
     with pytest.raises(IntegrityError):
         db_session.commit()
 
+def test_user_IntegrityError_when_external_id_is_blank_string(db_session: Session):
+    """
+        GIVEN a User model
+        AND external_id is null
+        WHEN User model is committed
+        THEN an IntegrityError is raised
+    """
+    # create a user
+    user_to_create = User(external_id='')
+    # attempt to commit the user
+    db_session.add(user_to_create)
+    with pytest.raises(IntegrityError):
+        db_session.commit()
+
 def test_get_user_by_primary_key(db_session: Session):
     """
         GIVEN a User model
@@ -166,8 +180,33 @@ def test_external_id_can_be_updated(db_session: Session):
     # assert that the values have reverted to their original state
     assert user.external_id != original_some_external_id
 
+
+def test_external_id_can_be_updated(db_session: Session):
+    """
+        GIVEN a User exists in the database
+        WHEN mutable-only fields ('external_id') are changed on the object
+        AND the session is committed
+        THEN the original values in the database is changed
+    """
+    # create a user
+    user = User(external_id="some_external_id")
+    db_session.add(user)
+    db_session.commit()
+    db_session.refresh(user)
+    # store the original value
+    original_some_external_id = user.external_id
+    # change the read-only fields in memory
+    user.external_id = 'oh_wow_this_is_some_external_id'
+    # commit the session.
+    # SQLAlchemy will see the object is "dirty"
+    # but should ignore changes to the primary key and non-writable fields
+    db_session.commit()
+    # refresh the object to get the true state from the database
+    db_session.refresh(user)
+    # assert that the values have reverted to their original state
+    assert user.external_id != original_some_external_id
+
 # TODO: Test deleting a user and verifying it's no longer in the database.
 # TODO: Test what happens when trying to delete a user that doesn't exist.
-# TODO: Test creating a user with an empty string for external_id.
 # TODO: Test how the database handles an external_id that is unusually long.
 # TODO: Test querying with an invalid UUID format to ensure it fails gracefully.
