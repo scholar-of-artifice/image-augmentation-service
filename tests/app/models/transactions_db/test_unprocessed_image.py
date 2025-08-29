8a7fa5ae1290e95ff19789736aa1d715bf2b3677
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-29 20:34:58 +0100

    add - test_unprocessed_image_IntegrityError_when_user_id_is_null

--- tests/app/models/transactions_db/test_unprocessed_image.py
+++ tests/app/models/transactions_db/test_unprocessed_image.py
@@ -36,6 +36,24 @@ def test_unprocessed_image_is_valid(db_session: Session):
     assert isinstance(unprocessed_image.created_at, datetime)
     assert unprocessed_image.created_at.tzinfo == timezone.utc
 
+def test_unprocessed_image_IntegrityError_when_user_id_is_null(db_session: Session):
+    """
+        GIVEN an attempt to create an UnprocessedImage entry
+        AND the user_id is None
+        WHEN the entry is committed
+        THEN an IntegrityError should be raised
+    """
+    # create an unprocessed_image
+    unprocessed_image = UnprocessedImage(
+        user_id= None,
+        original_filename= "cool_image.png",
+        storage_filename="some_file_name.png"
+    )
+    # attempt to save the data
+    db_session.add(unprocessed_image)
+    with pytest.raises(IntegrityError):
+        db_session.commit()
+
 def test_unprocessed_image_IntegrityError_when_user_id_does_not_exist(db_session: Session):
     """
         GIVEN an attempt to create an UnprocessedImage entry
