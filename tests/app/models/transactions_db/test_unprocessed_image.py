bf02b7a84130af7ed3d4162b44825471f29ae692
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-29 20:33:03 +0100

    edit - remove comment

--- tests/app/models/transactions_db/test_unprocessed_image.py
+++ tests/app/models/transactions_db/test_unprocessed_image.py
@@ -161,7 +161,6 @@ def test_delete_unprocessed_image(db_session: Session):
 # TODO: Test accessing the parent user object from an image instance (image.user).
 # TODO: Test that a user's 'unprocessed_images' list is correctly populated.
 # TODO: Test fetching all images belonging to a specific user.
-# TODO: Test deleting an image and confirming it's removed from the database.
 # TODO: Test that deleting a User with associated images raises an IntegrityError.
 # TODO: Test that creating an image with a null user_id raises an IntegrityError.
 # TODO: Test creating an image with a null original_filename raises an IntegrityError.
