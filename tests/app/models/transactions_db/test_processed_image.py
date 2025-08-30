197a77e8631cfd95fb1756558997ae94446aa610
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-30 14:30:12 +0100

    edit - remove name field

--- tests/app/models/transactions_db/test_processed_image.py
+++ tests/app/models/transactions_db/test_processed_image.py
@@ -15,7 +15,7 @@ def test_processed_image_is_valid(db_session: Session):
         THEN it persists correctly
     """
     # create a user
-    user = User(external_id='some-1234-extr-0987-id45', name="Test User")
+    user = User(external_id='some-1234-extr-0987-id45')
     db_session.add(user)
     db_session.commit()
     # create an unprocessed_image
