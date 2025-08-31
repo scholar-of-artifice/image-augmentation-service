63d276a313f00792f3a501a64411df06a7163cae
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-08-31 14:43:01 +0100

    edit - remove test redefinition

--- tests/app/models/transactions_db/test_user.py
+++ tests/app/models/transactions_db/test_user.py
@@ -204,32 +204,6 @@ def test_external_id_can_be_updated(db_session: Session):
     # assert that the values have reverted to their original state
     assert user.external_id != original_some_external_id
 
-
-def test_external_id_can_be_updated(db_session: Session):
-    """
-        GIVEN a User exists in the database
-        WHEN mutable-only fields ('external_id') are changed on the object
-        AND the session is committed
-        THEN the original values in the database is changed
-    """
-    # create a user
-    user = User(external_id="some_external_id")
-    db_session.add(user)
-    db_session.commit()
-    db_session.refresh(user)
-    # store the original value
-    original_some_external_id = user.external_id
-    # change the read-only fields in memory
-    user.external_id = 'oh_wow_this_is_some_external_id'
-    # commit the session.
-    # SQLAlchemy will see the object is "dirty"
-    # but should ignore changes to the primary key and non-writable fields
-    db_session.commit()
-    # refresh the object to get the true state from the database
-    db_session.refresh(user)
-    # assert that the values have reverted to their original state
-    assert user.external_id != original_some_external_id
-
 def test_delete_user(db_session: Session):
     """
         GIVEN a User exists in the database
