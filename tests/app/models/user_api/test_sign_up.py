e689fe735d5243678dec999a4fa996a37534a102
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-01 17:17:30 +0100

    add - test_ValidationError_when_user_read_is_missing_created_at

--- tests/app/models/user_api/test_sign_up.py
+++ tests/app/models/user_api/test_sign_up.py
@@ -47,3 +47,26 @@ def test_ValidationError_when_user_read_is_missing_id():
     assert user.id == user_id
     assert user.external_id == external_id
     assert user.created_at == time_created
+
+def test_ValidationError_when_user_read_is_missing_created_at():
+    """
+        GIVEN a uuid
+        AND no time
+        AND an external_id
+        WHEN a UserRead is created
+        THEN it raises an ValidationError
+    """
+    # input variables
+    user_id = uuid.uuid4()
+    time_created = None
+    external_id = "auth0|some_unique_id_123"
+    # create an instance of the model
+    user = UserRead(
+        id=user_id,
+        external_id=external_id,
+        created_at=time_created,
+    )
+    # assert that the fields were set correctly
+    assert user.id == user_id
+    assert user.external_id == external_id
+    assert user.created_at == time_created
