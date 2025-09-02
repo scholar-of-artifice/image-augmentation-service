c71eb6fca1d55055a0eb767a4ba5e5a2101a737a
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-02 13:43:19 +0100

    add - create.py module instead of sign_up.py. hated the last name

---
+++ app/models/user_api/create.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, ConfigDict
+import uuid
+from datetime import datetime
+
+class UserRead(BaseModel):
+    """
+        This is the model that will be returned to the client.
+        It includes the fields that are safe to expose.
+    """
+    # The user's unique internal identifier in the database.
+    id: uuid.UUID
+    # The user's unique identifier in the external authentication service.
+    external_id: str
+    # The timestamp indicating when the user record was first created.
+    created_at: datetime
+    # This tells Pydantic to read the data from ORM model attributes,
+    # So it can read data from `db_user.id`.
+    model_config = ConfigDict(from_attributes=True)
