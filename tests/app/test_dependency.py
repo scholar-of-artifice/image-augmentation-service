86529f41602101490b181cd93205c9a00fdb136f
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-02 12:47:23 +0100

    add - test_get_current_external_user_id_raise_HTTPException_when_external_id_is_not_present

--- tests/app/test_dependency.py
+++ tests/app/test_dependency.py
@@ -1,5 +1,6 @@
 import pytest
 import uuid
+from fastapi import HTTPException, status
 from app.dependency import get_current_external_user_id
 
 pytestmark = pytest.mark.asyncio
@@ -30,3 +31,18 @@ async def test_get_current_external_user_id_success_when_external_id_is_present(
     result = await get_current_external_user_id(external_id=test_external_id)
     # check that the returned value is what we expect.
     assert result == test_external_id
+
+async def test_get_current_external_user_id_raise_HTTPException_when_external_id_is_not_present():
+    """
+        GIVEN a user_id is not in the header
+        WHEN get_current_external_user_id
+        THEN it raises an HTTPException
+        AND has particular details
+    """
+    # define a sample external ID.
+    test_external_id = None
+    with pytest.raises(HTTPException) as exc:
+        # call the dependency function directly, passing the test ID.
+        await get_current_external_user_id(external_id=test_external_id)
+    assert exc.value.status_code == status.HTTP_401_UNAUTHORIZED
+    assert "Missing X-External-User-ID header" in str(exc.value.detail)
