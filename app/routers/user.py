7b0d7fd76780e8f2de960c087ac24c1dd426b66f
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-02 14:11:56 +0100

    add - file for handling user endpoint

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,8 +1,10 @@
-from fastapi import (APIRouter, status)
-from app.models.health_api.user import User
-from app.models.logging import LogEntry
 import logging
-from datetime import datetime
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlmodel import Session, select
+from app.internal.database import get_session
+from app.models.transactions_db.user import User
+from app.models.user_api.create import UserRead
+from app.dependency import get_current_external_user_id
 
 router = APIRouter()
 # set up logging
