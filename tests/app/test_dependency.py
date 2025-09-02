e2fdd6c1fb935ede74844da39a784658fb3d8e19
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-02 12:46:17 +0100

    add - test_get_current_external_user_id_success_when_external_id_is_present

--- tests/app/test_dependency.py
+++ tests/app/test_dependency.py
@@ -1,4 +1,5 @@
 import pytest
+import uuid
 from app.dependency import get_current_external_user_id
 
 pytestmark = pytest.mark.asyncio
@@ -11,6 +12,20 @@ async def test_get_current_external_user_id_success_when_external_id_is_present(
     """
     # define a sample external ID.
     test_external_id = "user-abc-123"
+    # call the dependency function directly, passing the test ID.
+    result = await get_current_external_user_id(external_id=test_external_id)
+    # check that the returned value is what we expect.
+    assert result == test_external_id
+
+async def test_get_current_external_user_id_success_when_external_id_is_present():
+    """
+        GIVEN a user_id is in the header
+        AND external_id is a more realistic example
+        WHEN get_current_external_user_id
+        THEN it returns the user_id
+    """
+    # define a sample external ID.
+    test_external_id = str(uuid.uuid4())
     # call the dependency function directly, passing the test ID.
     result = await get_current_external_user_id(external_id=test_external_id)
     # check that the returned value is what we expect.
