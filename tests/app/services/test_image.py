8bfb4ff6bd8702b771bb2749d92e1427adf4e095
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-08 13:57:16 +0100

    edit - these are async functions. we will need to mark them all for that

--- tests/app/services/test_image.py
+++ tests/app/services/test_image.py
@@ -1,2 +1,6 @@
 from app.models.image_api.upload import UploadRequestBody, ShiftArguments, RotateArguments
 from app.services.image import process_and_save_image
+
+import pytest
+
+pytestmark = pytest.mark.asyncio
