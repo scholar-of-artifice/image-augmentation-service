45552507822097014814daeb63db2fcdc3982acd
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-08 14:13:34 +0100

    edit -  use dependencies in process_and_save_image

--- app/services/image.py
+++ app/services/image.py
@@ -1,12 +1,19 @@
 from app.models.image_api.upload import UploadRequestBody, ImageProcessResponse
 from fastapi import UploadFile
+from typing import Callable
 from app.internal.file_handling import translate_file_to_numpy_array, write_numpy_array_to_image_file, create_file_name
 from app.internal.augmentations import shift, rotate
 
 async def process_and_save_image(
         file: UploadFile,
-        validated_data: UploadRequestBody
-    ):
+        validated_data: UploadRequestBody,
+        # --- INJECTED DEPENDENCIES ---
+        file_translator: Callable = translate_file_to_numpy_array,
+        file_writer: Callable = write_numpy_array_to_image_file,
+        filename_creator: Callable = create_file_name,
+        shift_processor: Callable = shift,      # TODO: this is probably changing later
+        rotate_processor: Callable = rotate,    # TODO: this is probably changing later
+    ) -> ImageProcessResponse:
     """
         Handles the core logic of processing and saving an image.
     """
@@ -14,9 +21,9 @@ async def process_and_save_image(
     # asynchronously read the contents of the uploaded file as bytes
     image_content = await file.read()
     # convert the raw image bytes into a numpy array
-    image_data = translate_file_to_numpy_array(image_content)
+    image_data = file_translator(image_content)
     # save a copy of the original unprocessed image to the 'unprocessed_image_data' volume.
-    unprocessed_image_location = write_numpy_array_to_image_file(
+    unprocessed_image_location = file_writer(
         data=image_data,
         file_name=file.filename,
         destination_volume='unprocessed_image_data'
@@ -27,21 +34,21 @@ async def process_and_save_image(
     # check the processing argument from the request to determine which action to take
     if validated_data.arguments.processing == "shift":
         # apply shift
-        new_img_data = shift(
+        new_img_data = shift_processor(
             image_data=image_data,
             direction=validated_data.arguments.direction,
             distance=validated_data.arguments.distance
         )
     elif validated_data.arguments.processing == "rotate":
         # appy rotate
-        new_img_data = rotate(
+        new_img_data = rotate_processor(
             image_data=image_data,
             angle=validated_data.arguments.angle
         )
     # save the processed image to the 'processed_image_data' volume.
-    processed_image_location = write_numpy_array_to_image_file(
+    processed_image_location = file_writer(
         data=new_img_data,
-        file_name=create_file_name(),
+        file_name=filename_creator(),
         destination_volume='processed_image_data'
     )
     # return an ImageProcessResponse
