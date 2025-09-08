80f1bcc90b2f992c67bf62c42b4db5970640ae3f
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-08 14:05:48 +0100

    edit - mock the dependencies test_process_and_save_image_with_shift_arguments_succeeds

--- tests/app/services/test_image.py
+++ tests/app/services/test_image.py
@@ -15,3 +15,9 @@ async def test_process_and_save_image_with_shift_arguments_succeeds(mocker):
         AND the rotate_processor is NOT called
         AND a valid ImageProcessResponse is returned
     """
+    # mock the dependencies
+    mock_file_translator = mocker.MagicMock(return_value="numpy_array_data")
+    mock_file_writer = mocker.MagicMock(side_effect=["/path/original.jpg", "/path/shifted.jpg"])
+    mock_filename_creator = mocker.MagicMock(return_value="new_filename.jpg")
+    mock_shift_processor = mocker.MagicMock(return_value="shifted_numpy_array")
+    mock_rotate_processor = mocker.MagicMock()
