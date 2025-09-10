7b18aab3d427f0e10ba3c198af9a3913efedf067
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-10 11:47:16 +0100

    edit - add division for get_current_active_user

--- tests/unit/app/test_dependency.py
+++ tests/unit/app/test_dependency.py
@@ -109,3 +109,4 @@ async def test_get_body_as_model_raises_unprocessable_entity_when_model_not_corr
     assert isinstance(exc.value.detail, list)
     assert len(exc.value.detail) > 0
 
+# --- get_current_active_user ---
