620585bf9e512fad8da9468126d552837e56b1b9
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-10 15:45:38 +0100

    add - test_upload_endpoint_service_error_fails_with_500

--- tests/integration/app/routers/test_image.py
+++ tests/integration/app/routers/test_image.py
@@ -64,3 +64,32 @@ async def test_upload_endpoint_missing_body_fails_with_422(mocker):
         )
         # Assert that FastAPI caught the missing data and returned a 422
         assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+async def test_upload_endpoint_service_error_fails_with_500(mocker):
+    """
+        GIVEN a valid file and request body
+        BUT the image processing service raises an unexpected error
+        WHEN a POST request is made to /upload
+        THEN a 500 Internal Server Error response is returned
+    """
+    shift_args = ShiftArguments(processing='shift', direction='right', distance=25)
+    request_body = UploadRequestBody(arguments=shift_args)
+    # mock the service function to raise an exception instead of returning a value
+    mocked_service = mocker.patch(
+        'app.routers.image.process_and_save_image',
+        side_effect=OSError("Something went wrong with saving the file")
+    )
+    async with AsyncClient(
+            transport=ASGITransport(app=app),
+            base_url="http://test"
+    ) as ac:
+        response = await ac.post(
+            url="/upload/",
+            files={"file": ('test.png', b'fake_image_bytes', 'image/png')},
+            data={'body': request_body.model_dump_json()}
+        )
+        # assert that the endpoint handled the service error correctly
+        assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        # check the content of the error message
+        assert response.json() == {"detail": "Internal Server Error"}
+        mocked_service.assert_called_once()
