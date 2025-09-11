45cd399a2b66c475fff513fe22f00e507f97c4b4
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-11 10:55:38 +0100

    edit - make service async

--- app/services/image.py
+++ app/services/image.py
@@ -1,14 +1,18 @@
+import uuid
 from app.schemas.image import UploadRequestBody, ImageProcessResponse
 from fastapi import UploadFile
 from typing import Callable
 from app.internal.file_handling import translate_file_to_numpy_array, write_numpy_array_to_image_file, create_file_name
 from app.internal.augmentations import shift, rotate
-from sqlmodel import Session
+from app.schemas.transactions_db.unprocessed_image import UnprocessedImage
+from app.schemas.transactions_db.processed_image import ProcessedImage
+from sqlalchemy.ext.asyncio import AsyncSession
 
 async def process_and_save_image(
         file: UploadFile,
         validated_data: UploadRequestBody,
-        db_session: Session,
+        db_session: AsyncSession,
+        user_id: uuid.UUID,
         # --- INJECTED DEPENDENCIES ---
         file_translator: Callable = translate_file_to_numpy_array,
         file_writer: Callable = write_numpy_array_to_image_file,
@@ -24,14 +28,20 @@ async def process_and_save_image(
     image_content = await file.read()
     # convert the raw image bytes into a numpy array
     image_data = file_translator(image_content)
+    # --- Save Relevant Unprocessed Image Data ---
+    unprocessed_storage_filename = filename_creator()
+    unprocessed_image_record = UnprocessedImage(
+        original_filename=file.filename,                # use the original name from the file
+        storage_filename=unprocessed_storage_filename,  # use a unique name
+        user_id=user_id                                 # associate to the user_id
+    )
     # save a copy of the original unprocessed image to the 'unprocessed_image_data' volume.
     unprocessed_image_location = file_writer(
         data=image_data,
-        file_name=file.filename,
+        file_name=unprocessed_storage_filename,
         destination_volume='unprocessed_image_data'
     )
-    # TODO: save the file metadata to PostgreSQL
-    # process the image
+    # --- Process the Image ---
     # initialize a new variables for the processed image data
     new_img_data = image_data
     # check the processing argument from the request to determine which action to take
@@ -48,12 +58,26 @@ async def process_and_save_image(
             image_data=image_data,
             angle=validated_data.arguments.angle
         )
+    # --- Save Relevant Processed Image Data ---
+    processed_storage_filename = filename_creator()
+    processed_image_record = ProcessedImage(
+        storage_filename=processed_storage_filename,  # use a unique name
+    )
     # save the processed image to the 'processed_image_data' volume.
     processed_image_location = file_writer(
         data=new_img_data,
-        file_name=filename_creator(),
+        file_name=processed_storage_filename,
         destination_volume='processed_image_data'
     )
+    # --- Link the Records ---
+    # link the processed image back to its unprocessed parent.
+    unprocessed_image_record.processed_images.append(processed_image_record)
+    # --- Persist to Database ---
+    # persist the data
+    db_session.add(unprocessed_image_record) # only need to add the parent; SQLAlchemy handles the rest
+    await db_session.commit()
+    await db_session.refresh(unprocessed_image_record)
+    await db_session.refresh(processed_image_record)
     # return an ImageProcessResponse
     return ImageProcessResponse(
         original_stored_file_path=unprocessed_image_location,
