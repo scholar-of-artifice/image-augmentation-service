3926e4546003d93b24c6ea5ed13d853eb9cd6536
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-12 10:12:27 +0100

    edit - define a lifespan even to create db_and_tables when app starts up

--- app/main.py
+++ app/main.py
@@ -2,7 +2,16 @@ from fastapi import FastAPI
 from pathlib import Path
 import logging.config
 import json
+from app.db.database import create_db_and_tables
 from app.routers import image, health, user
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("creating database and tables...")
+    create_db_and_tables()
+    yield
+    print('application shutdown.')
 
 def set_up_logging():
     config_file = Path(__file__).parent / "logging_config.json"
@@ -11,7 +20,7 @@ def set_up_logging():
     logging.config.dictConfig(config)
 
 set_up_logging()
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 logger = logging.getLogger(__name__)
 
 app.include_router(image.router, prefix="/image-api")
