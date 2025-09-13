d8cab39968e8c8f6f29400ba81e97a40cf171e8a
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-13 09:32:13 +0100

    edit - uv run ruff format tests/unit/app/internal

--- tests/unit/app/internal/test_augmentations.py
+++ tests/unit/app/internal/test_augmentations.py
@@ -3,24 +3,58 @@ import pytest
 from app.internal.augmentations import shift, rotate
 
 
-@pytest.mark.parametrize("direction, distance, expected_output", [
-    ('up', 2, numpy.array([[0, 0, 3, 0,],
-                           [0, 0, 0, 4,],
-                           [1, 0, 0, 0,],
-                           [0, 2, 0, 0,]])),
-    ('down', 2, numpy.array([[0, 0, 3, 0],
-                             [0, 0, 0, 4],
-                             [1, 0, 0, 0],
-                             [0, 2, 0, 0]])),
-    ('left', 2, numpy.array([[0, 0, 1, 0],
-                             [0, 0, 0, 2],
-                             [3, 0, 0, 0],
-                             [0, 4, 0, 0]])),
-    ('right', 2, numpy.array([[0, 0, 1, 0],
-                              [0, 0, 0, 2],
-                              [3, 0, 0, 0],
-                              [0, 4, 0, 0]])),
-])
+@pytest.mark.parametrize(
+    "direction, distance, expected_output",
+    [
+        (
+            "up",
+            2,
+            numpy.array(
+                [
+                    [
+                        0,
+                        0,
+                        3,
+                        0,
+                    ],
+                    [
+                        0,
+                        0,
+                        0,
+                        4,
+                    ],
+                    [
+                        1,
+                        0,
+                        0,
+                        0,
+                    ],
+                    [
+                        0,
+                        2,
+                        0,
+                        0,
+                    ],
+                ]
+            ),
+        ),
+        (
+            "down",
+            2,
+            numpy.array([[0, 0, 3, 0], [0, 0, 0, 4], [1, 0, 0, 0], [0, 2, 0, 0]]),
+        ),
+        (
+            "left",
+            2,
+            numpy.array([[0, 0, 1, 0], [0, 0, 0, 2], [3, 0, 0, 0], [0, 4, 0, 0]]),
+        ),
+        (
+            "right",
+            2,
+            numpy.array([[0, 0, 1, 0], [0, 0, 0, 2], [3, 0, 0, 0], [0, 4, 0, 0]]),
+        ),
+    ],
+)
 def test_shift_produces_correct_results(direction, distance, expected_output):
     """
     GIVEN a 4x4 matrix
@@ -28,12 +62,10 @@ def test_shift_produces_correct_results(direction, distance, expected_output):
     WHEN shift is called
     THEN the new matrix has the correct value
     """
-    input_image = numpy.array([[1, 0, 0, 0],
-                               [0, 2, 0, 0],
-                               [0, 0, 3, 0],
-                               [0, 0, 0, 4]])
-    calculated_output = shift(image_data=input_image,
-                              direction=direction, distance=distance)
+    input_image = numpy.array([[1, 0, 0, 0], [0, 2, 0, 0], [0, 0, 3, 0], [0, 0, 0, 4]])
+    calculated_output = shift(
+        image_data=input_image, direction=direction, distance=distance
+    )
     print(calculated_output)
     assert numpy.array_equal(calculated_output, expected_output)
 
@@ -46,12 +78,9 @@ def test_shift_invalid_direction_raises_exception():
     WHEN shift is called
     THEN it raises a ValueError
     """
-    input_image = numpy.array([[1, 0, 0, 0],
-                               [0, 2, 0, 0],
-                               [0, 0, 3, 0],
-                               [0, 0, 0, 4]])
+    input_image = numpy.array([[1, 0, 0, 0], [0, 2, 0, 0], [0, 0, 3, 0], [0, 0, 0, 4]])
     with pytest.raises(ValueError):
-        shift(input_image, 'whichaway', 3)
+        shift(input_image, "whichaway", 3)
 
 
 def test_shift_None_direction_raises_exception():
@@ -62,10 +91,7 @@ def test_shift_None_direction_raises_exception():
     WHEN shift is called
     THEN it raises a TypeError
     """
-    input_image = numpy.array([[1, 0, 0, 0],
-                               [0, 2, 0, 0],
-                               [0, 0, 3, 0],
-                               [0, 0, 0, 4]])
+    input_image = numpy.array([[1, 0, 0, 0], [0, 2, 0, 0], [0, 0, 3, 0], [0, 0, 0, 4]])
     with pytest.raises(TypeError):
         shift(input_image, None, 3)
 
@@ -78,12 +104,9 @@ def test_shift_blank_string_direction_raises_exception():
     WHEN shift is called
     THEN it raises a ValueError
     """
-    input_image = numpy.array([[1, 0, 0, 0],
-                               [0, 2, 0, 0],
-                               [0, 0, 3, 0],
-                               [0, 0, 0, 4]])
+    input_image = numpy.array([[1, 0, 0, 0], [0, 2, 0, 0], [0, 0, 3, 0], [0, 0, 0, 4]])
     with pytest.raises(ValueError):
-        shift(input_image, '', 3)
+        shift(input_image, "", 3)
 
 
 def test_shift_float_distance_raises_exception():
@@ -94,12 +117,10 @@ def test_shift_float_distance_raises_exception():
     WHEN shift is called
     THEN it raises a TypeError
     """
-    input_image = numpy.array([[1, 0, 0, 0],
-                               [0, 2, 0, 0],
-                               [0, 0, 3, 0],
-                               [0, 0, 0, 4]])
+    input_image = numpy.array([[1, 0, 0, 0], [0, 2, 0, 0], [0, 0, 3, 0], [0, 0, 0, 4]])
     with pytest.raises(TypeError):
-        shift(input_image, 'up', 3.5)
+        shift(input_image, "up", 3.5)
+
 
 def test_shift_bad_input_dimensions_raises_exception():
     """
@@ -110,7 +131,7 @@ def test_shift_bad_input_dimensions_raises_exception():
     """
     input_image = numpy.array([1, 0, 0, 0])
     with pytest.raises(TypeError):
-        shift(input_image, 'left', 3)
+        shift(input_image, "left", 3)
 
 
 def test_rotate_example_45_degrees():
@@ -120,14 +141,10 @@ def test_rotate_example_45_degrees():
     WHEN rotate is called
     THEN the new matrix has the correct value
     """
-    input_image = numpy.array([[1, 0, 0, 0],
-                               [0, 2, 0, 0],
-                               [0, 0, 3, 0],
-                               [0, 0, 0, 4]])
-    expected_output = numpy.array([[0, 0, 0, 0],
-                                   [0, 1, 1, 1],
-                                   [0, 1, 1, 1],
-                                   [0, 0, 0, 0]])
+    input_image = numpy.array([[1, 0, 0, 0], [0, 2, 0, 0], [0, 0, 3, 0], [0, 0, 0, 4]])
+    expected_output = numpy.array(
+        [[0, 0, 0, 0], [0, 1, 1, 1], [0, 1, 1, 1], [0, 0, 0, 0]]
+    )
     calculated_output = rotate(input_image, 45)
     assert numpy.array_equal(calculated_output, expected_output)
 
@@ -139,30 +156,39 @@ def test_rotate_example_90_degrees():
     WHEN rotate is called
     THEN the new matrix has the correct value
     """
-    input_image = numpy.array([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                               [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                               [0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0],
-                               [0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0],
-                               [0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0],
-                               [0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0],
-                               [0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0],
-                               [0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0],
-                               [0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0],
-                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0],
-                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11]])
-    expected_output = numpy.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11],
-                                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0],
-                                  [0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0],
-                                  [0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0],
-                                  [0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0],
-                                  [0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0],
-                                  [0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0],
-                                  [0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0],
-                                  [0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0],
-                                  [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                                  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+    input_image = numpy.array(
+        [
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11],
+        ]
+    )
+    expected_output = numpy.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
     calculated_output = rotate(input_image, 90)
     assert numpy.array_equal(calculated_output, expected_output)
+
 
 def test_rotate_example_0_degrees():
     """
@@ -171,30 +197,39 @@ def test_rotate_example_0_degrees():
     WHEN rotate is called
     THEN the new matrix has the correct value
     """
-    input_image = numpy.array([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                               [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                               [0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0],
-                               [0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0],
-                               [0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0],
-                               [0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0],
-                               [0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0],
-                               [0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0],
-                               [0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0],
-                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0],
-                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11]])
-    expected_output = numpy.array([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                               [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                               [0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0],
-                               [0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0],
-                               [0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0],
-                               [0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0],
-                               [0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0],
-                               [0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0],
-                               [0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0],
-                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0],
-                               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11]])
+    input_image = numpy.array(
+        [
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11],
+        ]
+    )
+    expected_output = numpy.array(
+        [
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11],
+        ]
+    )
     calculated_output = rotate(input_image, 0)
     assert numpy.array_equal(calculated_output, expected_output)
+
 
 def test_rotate_bad_input_dimensions_raises_exception():
     """
@@ -207,6 +242,7 @@ def test_rotate_bad_input_dimensions_raises_exception():
     with pytest.raises(TypeError):
         rotate(input_image, angle=45)
 
+
 def test_rotate_angle_of_string_raises_exception():
     """
     GIVEN a 4x4 matrix
@@ -214,9 +250,6 @@ def test_rotate_angle_of_string_raises_exception():
     WHEN rotate is called
     THEN it raises a TypeError
     """
-    input_image = numpy.array([[1, 0, 0, 0],
-                               [0, 2, 0, 0],
-                               [0, 0, 3, 0],
-                               [0, 0, 0, 4]])
+    input_image = numpy.array([[1, 0, 0, 0], [0, 2, 0, 0], [0, 0, 3, 0], [0, 0, 0, 4]])
     with pytest.raises(TypeError):
-        rotate(input_image, angle='45')
+        rotate(input_image, angle="45")
--- tests/unit/app/internal/test_file_handling.py
+++ tests/unit/app/internal/test_file_handling.py
@@ -3,69 +3,78 @@ import pytest
 from app.internal.file_handling import (
     translate_file_to_numpy_array,
     InvalidImageFileError,
-    create_file_name
+    create_file_name,
 )
 from tests.helperfunc import create_dummy_image_bytes
 
+
 def test_translate_file_to_numpy_array_creates_correct_result_when_given_valid_image_data():
     """
-        GIVEN valid image bytes
-        WHEN translate_file_to_numpy_array is called
-        THEN the correct numpy array is returned.
+    GIVEN valid image bytes
+    WHEN translate_file_to_numpy_array is called
+    THEN the correct numpy array is returned.
     """
     input_image_bytes = create_dummy_image_bytes()
-    expected_output = numpy.array( object= [
-        [[255, 0, 0], [0,255, 0], [0, 0, 255]],
-        [[255, 255, 255], [127, 127, 127], [0, 0, 0]],
-        [[127, 0, 0], [0,127, 0], [0, 0, 127]],
-    ], dtype=numpy.uint8 )
-    calculated_output = translate_file_to_numpy_array( content= input_image_bytes )
+    expected_output = numpy.array(
+        object=[
+            [[255, 0, 0], [0, 255, 0], [0, 0, 255]],
+            [[255, 255, 255], [127, 127, 127], [0, 0, 0]],
+            [[127, 0, 0], [0, 127, 0], [0, 0, 127]],
+        ],
+        dtype=numpy.uint8,
+    )
+    calculated_output = translate_file_to_numpy_array(content=input_image_bytes)
     assert numpy.array_equal(expected_output, calculated_output)
+
 
 def test_translate_file_to_numpy_array_raises_InvalidImageFileError_when_given_invalid_image_data():
     """
-        GIVEN invalid image bytes
-        WHEN translate_file_to_numpy_array is called
-        THEN an exception is raised.
+    GIVEN invalid image bytes
+    WHEN translate_file_to_numpy_array is called
+    THEN an exception is raised.
     """
     # make bad data
-    input_image_bytes = b'this is not a valid image bytes'
+    input_image_bytes = b"this is not a valid image bytes"
     # assert that calling the function raises an error
     with pytest.raises(InvalidImageFileError):
-        translate_file_to_numpy_array( content= input_image_bytes )
+        translate_file_to_numpy_array(content=input_image_bytes)
+
 
 def test_create_file_name_returns_a_string():
     """
-        GIVEN no arguments
-        WHEN create_file_name is called
-        THEN it should return a string
+    GIVEN no arguments
+    WHEN create_file_name is called
+    THEN it should return a string
     """
     assert isinstance(create_file_name(), str)
 
+
 def test_create_file_name_returns_a_non_empty_string():
     """
-        GIVEN no arguments
-        WHEN create_file_name is called
-        THEN it should return a non-empty string
+    GIVEN no arguments
+    WHEN create_file_name is called
+    THEN it should return a non-empty string
     """
     calculated_file_name = create_file_name()
-    assert calculated_file_name != 'wow_a_file.png'
+    assert calculated_file_name != "wow_a_file.png"
+
 
 def test_create_file_name_returns_a_nonspecific_string():
     """
-        GIVEN no arguments
-        WHEN create_file_name is called
-        THEN it should return a non-empty string
+    GIVEN no arguments
+    WHEN create_file_name is called
+    THEN it should return a non-empty string
     """
     calculated_file_name = create_file_name()
-    assert calculated_file_name != 'wow_an_oddly_specific_filename.png'
+    assert calculated_file_name != "wow_an_oddly_specific_filename.png"
+
 
 def test_create_file_name_returns_a_different_values_per_function_call():
     """
-        GIVEN no arguments
-        WHEN create_file_name is called twice
-        THEN it should return different values
+    GIVEN no arguments
+    WHEN create_file_name is called twice
+    THEN it should return different values
     """
     calculated_file_name_A = create_file_name()
     calculated_file_name_B = create_file_name()
-    assert(calculated_file_name_A != calculated_file_name_B)
+    assert calculated_file_name_A != calculated_file_name_B
