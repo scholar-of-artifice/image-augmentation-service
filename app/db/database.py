1c33015fb7dc5467a6b3aa978bb0b9ec2df8f95b
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-15 13:21:24 +0100

    edit - uv run ruff format app/db/database.py

--- app/db/database.py
+++ app/db/database.py
@@ -7,27 +7,33 @@ from app.config import settings
 # Pydantic handles the os.getenv() part for you
 engine = create_engine(str(settings.DATABASE_URL), echo=True)
 
+
 def create_db_and_tables():
     """
-        Creates the database and tables.
+    Creates the database and tables.
     """
     SQLModel.metadata.create_all(engine)
 
+
 def get_session():
     """
-        Creates a new database session and returns the session.
+    Creates a new database session and returns the session.
     """
     with Session(engine) as session:
         yield session
 
+
 # Asynchronous session
 
-async_db_url = str(settings.DATABASE_URL).replace("postgresql://", "postgresql+psycopg://")
+async_db_url = str(settings.DATABASE_URL).replace(
+    "postgresql://", "postgresql+psycopg://"
+)
 async_engine = create_async_engine(async_db_url, echo=True)
+
 
 async def get_async_session():
     """
-        Creates a new async database session and returns the session.
+    Creates a new async database session and returns the session.
     """
     async_session = sessionmaker(
         bind=async_engine, class_=AsyncSession, expire_on_commit=False
