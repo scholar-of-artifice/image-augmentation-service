46acfe5b3b7ee633f5a487a393fa886818e4cb56
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-21 20:10:21 +0100

    edit - define async client fixture scope to function... maybe module in future... idk

--- tests/end_to_end/conftest.py
+++ tests/end_to_end/conftest.py
@@ -5,10 +5,10 @@ import pytest_asyncio
 
 BASE_URL = os.getenv("API_BASE_URL")
 
-@pytest_asyncio.fixture(scope="module")
+@pytest_asyncio.fixture(scope="function")
 async def http_client():
     """
-        Provides an async httpx client for the test module.
+        Provides an async httpx client for each test function.
     """
     async with httpx.AsyncClient(base_url=BASE_URL) as client:
         yield client
