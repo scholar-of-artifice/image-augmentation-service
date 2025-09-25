719f7f9ff8e638d6d8e2817b67655384d8e9d351
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-25 12:59:21 +0100

    edit - make tests for transactions_db/user async

--- tests/integration/app/schemas/transactions_db/test_user.py
+++ tests/integration/app/schemas/transactions_db/test_user.py
@@ -9,8 +9,9 @@ from sqlmodel import select
 
 from app.schemas.transactions_db.user import User
 
+pytestmark = pytest.mark.asyncio
 
-def test_user_valid_model_is_persisted(db_session: AsyncSession):
+async def test_user_valid_model_is_persisted(async_db_session: AsyncSession):
     """
     GIVEN a User model
     AND all input data is valid
@@ -20,9 +21,9 @@ def test_user_valid_model_is_persisted(db_session: AsyncSession):
     """
     # create a user
     user = User(external_id="some-1234-extr-0987-id45")
-    db_session.add(user)
-    db_session.commit()
-    db_session.refresh(user)
+    async_db_session.add(user)
+    await async_db_session.commit()
+    await async_db_session.refresh(user)
     # Assert that the database populated the fields correctly
     assert isinstance(user.id, uuid.UUID)
     assert user.id is not None
@@ -34,7 +35,7 @@ def test_user_valid_model_is_persisted(db_session: AsyncSession):
     assert user.created_at.tzinfo == UTC
 
 
-def test_user_IntegrityError_when_external_id_is_duplicate(db_session: AsyncSession):
+async def test_user_IntegrityError_when_external_id_is_duplicate(async_db_session: AsyncSession):
     """
     GIVEN a User A already exists
     AND a new User B is created
@@ -46,14 +47,14 @@ def test_user_IntegrityError_when_external_id_is_duplicate(db_session: AsyncSess
     user_A = User(external_id="some-1234-extr-0987-id45")
     user_B = User(external_id="some-1234-extr-0987-id45")
     # add the entries to the session
-    db_session.add(user_A)
-    db_session.add(user_B)
+    async_db_session.add(user_A)
+    async_db_session.add(user_B)
     # attempt commit them
     with pytest.raises(IntegrityError):
-        db_session.flush()
+        await async_db_session.flush()
 
 
-def test_user_IntegrityError_when_external_id_is_null(db_session: AsyncSession):
+async def test_user_IntegrityError_when_external_id_is_null(async_db_session: AsyncSession):
     """
     GIVEN a User model
     AND external_id is null
@@ -63,12 +64,12 @@ def test_user_IntegrityError_when_external_id_is_null(db_session: AsyncSession):
     # create a user
     user_to_create = User(external_id=None)
     # attempt to commit the user
-    db_session.add(user_to_create)
+    async_db_session.add(user_to_create)
     with pytest.raises(IntegrityError):
-        db_session.flush()
+        await async_db_session.flush()
 
 
-def no_test_user_ValidationError_when_external_id_is_blank_string(db_session: AsyncSession):
+def no_test_user_ValidationError_when_external_id_is_blank_string(async_db_session: AsyncSession):
     # TODO: test fails and validation in pydantic not working as expected. take out for now.
     """
     GIVEN a User model
@@ -80,7 +81,7 @@ def no_test_user_ValidationError_when_external_id_is_blank_string(db_session: As
         User(external_id="")
 
 
-def test_user_IntegrityError_when_external_id_is_too_long(db_session: AsyncSession):
+async def test_user_IntegrityError_when_external_id_is_too_long(async_db_session: AsyncSession):
     """
     GIVEN a User model
     AND external_id is too long
@@ -90,12 +91,12 @@ def test_user_IntegrityError_when_external_id_is_too_long(db_session: AsyncSessi
     # create a user
     user_to_create = User(external_id="a" * 300)
     # attempt to commit the user
-    db_session.add(user_to_create)
+    async_db_session.add(user_to_create)
     with pytest.raises(DataError):
-        db_session.flush()
+        await async_db_session.flush()
 
 
-def test_get_user_by_primary_key(db_session: AsyncSession):
+async def test_get_user_by_primary_key(async_db_session: AsyncSession):
     """
     GIVEN a User model
     AND all input data is valid
@@ -104,18 +105,18 @@ def test_get_user_by_primary_key(db_session: AsyncSession):
     """
     # create a user
     user = User(external_id="this_is_some_external_id")
-    db_session.add(user)
-    db_session.commit()
-    db_session.refresh(user)
+    async_db_session.add(user)
+    await async_db_session.commit()
+    await async_db_session.refresh(user)
     # get the user by id
-    retrieved_user = db_session.get(User, user.id)
+    retrieved_user = await async_db_session.get(User, user.id)
     # assert that the users are the same
     assert retrieved_user is not None
     assert retrieved_user.id == user.id
     assert retrieved_user.external_id == "this_is_some_external_id"
 
 
-def test_get_user_by_primary_key_not_found(db_session: AsyncSession):
+async def test_get_user_by_primary_key_not_found(async_db_session: AsyncSession):
     """
     GIVEN no User exists with a specific ID
     WHEN a User is retrieved by that ID
@@ -124,12 +125,12 @@ def test_get_user_by_primary_key_not_found(db_session: AsyncSession):
     # make some UUID
     non_existent_id = uuid.uuid4()
     # try to get a user with that ID
-    retrieved_user = db_session.get(User, non_existent_id)
+    retrieved_user = await async_db_session.get(User, non_existent_id)
     # the result is None
     assert retrieved_user is None
 
 
-def test_get_user_by_external_id(db_session: AsyncSession):
+async def test_get_user_by_external_id(async_db_session: AsyncSession):
     """
     GIVEN a User exists in the database
     WHEN a query is made for the User by the external_id
@@ -138,18 +139,19 @@ def test_get_user_by_external_id(db_session: AsyncSession):
     # make a user with a known external_id
     target_external_id = "some_external_id"
     user_in_db = User(external_id=target_external_id)
-    db_session.add(user_in_db)
-    db_session.commit()
+    async_db_session.add(user_in_db)
+    await async_db_session.flush()
     # query for the user by that external_id
     statement = select(User).where(User.external_id == target_external_id)
-    retrieved_user = db_session.exec(statement).first()
+    result = await async_db_session.execute(statement)
+    retrieved_user = result.scalars().first()
     # the retrieved user should be the right one
     assert retrieved_user is not None
     assert retrieved_user.external_id == target_external_id
     assert retrieved_user.id == user_in_db.id
 
 
-def no_test_read_only_fields_are_not_updated(db_session: AsyncSession):
+async def NO_test_read_only_fields_are_not_updated(async_db_session: AsyncSession):
     # TODO: this test fails
     """
     GIVEN a User exists in the database
@@ -159,9 +161,9 @@ def no_test_read_only_fields_are_not_updated(db_session: AsyncSession):
     """
     # create a user
     user = User(external_id="some_external_id")
-    db_session.add(user)
-    db_session.commit()
-    db_session.refresh(user)
+    async_db_session.add(user)
+    async_db_session.commit()
+    async_db_session.refresh(user)
     # store the original, correct values
     original_id = user.id
     original_created_at = user.created_at
@@ -171,15 +173,15 @@ def no_test_read_only_fields_are_not_updated(db_session: AsyncSession):
     # commit the session.
     # SQLAlchemy will see the object is "dirty"
     # but should ignore changes to the primary key and non-writable fields
-    db_session.commit()
+    async_db_session.commit()
     # refresh the object to get the true state from the database
-    db_session.refresh(user)
+    async_db_session.refresh(user)
     # assert that the values have reverted to their original state
     assert user.id == original_id
     assert user.created_at == original_created_at
 
 
-def test_external_id_can_be_updated(db_session: AsyncSession):
+async def test_external_id_can_be_updated(async_db_session: AsyncSession):
     """
     GIVEN a User exists in the database
     WHEN mutable-only fields ('external_id') are changed on the object
@@ -188,9 +190,9 @@ def test_external_id_can_be_updated(db_session: AsyncSession):
     """
     # create a user
     user = User(external_id="some_external_id")
-    db_session.add(user)
-    db_session.commit()
-    db_session.refresh(user)
+    async_db_session.add(user)
+    await async_db_session.flush()
+    await async_db_session.refresh(user)
     # store the original value
     original_some_external_id = user.external_id
     # change the read-only fields in memory
@@ -198,14 +200,15 @@ def test_external_id_can_be_updated(db_session: AsyncSession):
     # commit the session.
     # SQLAlchemy will see the object is "dirty"
     # but should ignore changes to the primary key and non-writable fields
-    db_session.commit()
+    await async_db_session.flush()
     # refresh the object to get the true state from the database
-    db_session.refresh(user)
+    await async_db_session.refresh(user)
+    await async_db_session.commit()
     # assert that the values have reverted to their original state
     assert user.external_id != original_some_external_id
 
 
-def test_delete_user(db_session: AsyncSession):
+async def test_delete_user(async_db_session: AsyncSession):
     """
     GIVEN a User exists in the database
     WHEN the User is deleted
@@ -213,22 +216,22 @@ def test_delete_user(db_session: AsyncSession):
     """
     # create a user
     user_to_delete = User(external_id="some_external_id")
-    db_session.add(user_to_delete)
-    db_session.commit()
-    db_session.refresh(user_to_delete)
+    async_db_session.add(user_to_delete)
+    await async_db_session.commit()
+    await async_db_session.refresh(user_to_delete)
     # store its ID so we can look for it later
     user_id = user_to_delete.id
     # confirm it's in the database before deleting
-    assert db_session.get(User, user_id) is not None
+    assert await async_db_session.get(User, user_id) is not None
     # delete the user
-    db_session.delete(user_to_delete)
-    db_session.commit()
+    await async_db_session.delete(user_to_delete)
+    await async_db_session.commit()
     # the user cannot be found by its primary key
-    retrieved_user = db_session.get(User, user_id)
+    retrieved_user = await async_db_session.get(User, user_id)
     assert retrieved_user is None
 
 
-def test_delete_user_that_does_not_exist(db_session: AsyncSession):
+async def test_delete_user_that_does_not_exist(async_db_session: AsyncSession):
     """
     GIVEN a User does not exist in the database
     WHEN the User is deleted
@@ -241,7 +244,7 @@ def test_delete_user_that_does_not_exist(db_session: AsyncSession):
     # delete raises an error
     # because this instance is not tracked by the session.
     with pytest.raises(InvalidRequestError):
-        db_session.delete(user_not_in_db)
+        await async_db_session.delete(user_not_in_db)
 
 
 # TODO: Test querying with an invalid UUID format to ensure it fails gracefully.
