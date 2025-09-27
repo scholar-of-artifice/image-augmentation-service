9c5e213e68f510cac2194ba830cc7a7d752258a0
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-27 08:54:49 +0100

    add - get_unprocessed_image_by_id_endpoint

--- app/routers/image.py
+++ app/routers/image.py
@@ -1,12 +1,15 @@
+from pathlib import Path
+from app.internal.file_handling import VOLUME_PATHS
 from fastapi import APIRouter, Depends, UploadFile
+from fastapi.responses import FileResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.database import get_async_session
 from app.dependency.async_dependency import get_body_as_model, get_current_active_user
 from app.schemas.image import ImageProcessResponse, UploadRequestBody
 from app.schemas.transactions_db.user import User
-from app.services.image import process_and_save_image
-
+from app.services.image import process_and_save_image, get_unprocessed_image_by_id
+import uuid
 router = APIRouter()
 
 
@@ -33,4 +36,33 @@ async def upload_endpoint(
         db_session=db_session,
         user_id=current_user.id
     )
+
+@router.get(
+    path="unprocessed-image/{unprocessed_image_id}/",
+    response_class=FileResponse
+)
+async def get_unprocessed_image_by_id_endpoint(
+        unprocessed_image_id: uuid.UUID,
+        db_session: AsyncSession = Depends(get_async_session),
+        current_user: User = Depends(get_current_active_user)
+):
+    """
+        Get an unprocessed image by its ID.
+
+        Arguments:
+            unprocessed_image_id {str} -- The id of the unprocessed image.
+    """
+    image = await get_unprocessed_image_by_id(
+        unprocessed_image_id=unprocessed_image_id,
+        db_session=db_session,
+        user_id=current_user.id
+    )
+    image_path = VOLUME_PATHS["unprocessed_image_data"] / image.storage_filename
+
+    return FileResponse(
+        path=image_path,
+        media_type=image.media_type,
+        filename=image.storage_filename,
+    )
+
 
