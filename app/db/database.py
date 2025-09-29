10756e5945c3be511b65e854e4f6969643fcdc84
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-29 17:28:01 +0100

    edit - improve comments

--- app/db/database.py
+++ app/db/database.py
@@ -4,8 +4,8 @@ from sqlmodel import SQLModel, create_engine
 
 from app.config import settings
 
-# Use the validated DATABASE_URL directly from settings
-# Pydantic handles the os.getenv() part for you
+# call a function to get the application settings establish a connection to the
+# ... database.
 engine = create_engine(str(settings.DATABASE_URL), echo=True)
 
 
@@ -18,18 +18,33 @@ def create_db_and_tables():
 
 # Asynchronous session
 
+# takes the original database connection string and modifies it to create a new one
+# ... that's specifically for asynchronous database operations.
 async_db_url = str(settings.DATABASE_URL).replace(
-    "postgresql://", "postgresql+psycopg://"
+    "postgresql://", # look for this string
+    "postgresql+psycopg://" # replace with this string and use this driver
 )
-async_engine = create_async_engine(async_db_url, echo=True)
+
+# create a new database engine specifically for asynchronous communication.
+async_engine = create_async_engine(
+    async_db_url,
+    echo=True
+)
 
 
 async def get_async_session():
     """
     Creates a new async database session and returns the session.
     """
+    # use the session factory to make a new asynchronous session
     async_session = sessionmaker(
-        bind=async_engine, class_=AsyncSession, expire_on_commit=False
+        # the engine instance to use
+        bind=async_engine,
+        # the type of session
+        class_=AsyncSession,
+        # the session will close when a transaction is finished
+        expire_on_commit=False
     )
+    # closes the session when the block is exited
     async with async_session() as session:
-        yield session
+        yield session # makes a session available
