80934ba843c7debdc7172149e6360c6308ba2487
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-09-30 12:11:20 +0100

    fix - test_upload_an_image_to_shift

--- tests/end_to_end/test_upload_an_image_to_shift.py
+++ tests/end_to_end/test_upload_an_image_to_shift.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pytest
 from fastapi import status
 
-from app.schemas.image import ImageProcessResponse, ShiftArguments, UploadRequestBody
+from app.schemas.image import ResponseUploadImage, ShiftArguments, UploadRequestBody
 
 pytestmark = pytest.mark.asyncio
 
@@ -23,14 +23,11 @@ async def test_upload_an_image_to_shift(http_client):
     # --- UPLOAD AN IMAGE ---
     image_path = Path("/image-augmentation-service/tests/data/test_image.png")
     assert image_path.exists()
-    shift_args = ShiftArguments(processing="shift", direction="right", distance=25)
-    request_body = UploadRequestBody(arguments=shift_args)
     with open(image_path, "rb") as image_file:
         upload_response = await http_client.post(
             headers=headers,
             url="/image-api/upload/",
             files={"file": ("test.png", image_file, "image/png")},
-            data={"body": request_body.model_dump_json()},
         )
     assert upload_response.status_code == status.HTTP_200_OK
-    ImageProcessResponse.model_validate(upload_response.json())
+    ResponseUploadImage.model_validate(upload_response.json())
