d7da589ea4fce8ca1438d20adfd8771ea5fed03b
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-10-03 16:24:30 +0100

    edit - remove implementation and handle entirely within service layer call

--- app/routers/user.py
+++ app/routers/user.py
@@ -47,23 +47,10 @@ async def sign_up_endpoint(
     #   this is highly dependent on how you use this app.
     #   probably you should do that here where this comment is...
     # <--- NOTE
-    # call the service to check for an existing user
-    existing_user = await get_user_by_external_id(
+    return await sign_up_service(
         db_session=db_session,
         external_id=external_id
     )
-    # the endpoint handles the HTTP-specific logic
-    if existing_user:
-        # already have this user
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail=f"User with external_id '{external_id}' already exists."
-        )
-    # call the service to create the new user
-    new_user = await create_user(db_session=db_session, external_id=external_id)
-    # convert the SQLModel object to your Pydantic UserRead schema
-    # this only works because UserRead has `from_attributes=True` and SQLModel is Pydantic-compatible.
-    return UserRead.model_validate(new_user)
 
 @router.post(
     path="/sign-in",
