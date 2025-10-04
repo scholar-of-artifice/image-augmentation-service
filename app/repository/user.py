98af3c1a6aa1295f23d46e0e65784bf86fc0a1f9
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-10-04 20:32:40 +0100

    edit - move utility function to repository layer

---
+++ app/repository/user.py
@@ -0,0 +1,35 @@
+
+
+async def create_user(
+    external_id: str,
+    db_session: AsyncSession = Depends(get_async_session)
+) -> User:
+    """
+    - create a new user in the transaction database
+    - returns the newly created User object.
+    """
+    user_record = User(external_id=external_id)
+    db_session.add(user_record)
+    await db_session.flush()
+    await db_session.refresh(user_record)
+    await db_session.commit()
+    return user_record
+
+async def get_user_by_external_id(
+    external_id: str,
+    db_session: AsyncSession = Depends(get_async_session)
+) -> User | None:
+    """
+    Retrieves a user from the database by their external ID.
+    Returns the User object or None if not found.
+    This function should not raise an exception.or HTTPError.
+    """
+    result = await db_session.execute(
+        select(User).where(
+            User.external_id == external_id
+        )
+    )
+    user_record = result.scalars().first()
+    # --- return relevant information to user ---
+    return user_record
+
