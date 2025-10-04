11f42ac7326886d9e4cb50fb2b6983ff469739c6
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-10-04 09:33:42 +0100

    add - test  test_sign_up_a_user_403

---
+++ tests/end_to_end/test_sign_up_a_user_403.py
@@ -0,0 +1,23 @@
+import uuid
+
+import pytest
+from fastapi import status
+
+pytestmark = pytest.mark.asyncio
+
+async def test_sign_up_a_user_403(http_client):
+    """
+    This test checks auth. is possible for creating a User.
+    1. Create a user
+    """
+    external_id = ""
+    headers = {
+        "X-External-User-ID": external_id
+    }
+    # --- CREATE A USER ---
+    response = await http_client.post(
+        url="/users-api/sign-up",
+        headers=headers
+    )
+    # --- CHECK THE RESPONSE ---
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
