6cfe17ef4466f3f52b4bcf3e4b5c3ecabab11abb
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-10-05 09:29:13 +0100

    add - test_ImageNotFound_is_raised

---
+++ tests/unit/app/exceptions/test_image.py
@@ -0,0 +1,18 @@
+import pytest
+
+from app.exceptions.image import ImageNotFound
+
+def fake_ImageNotFound_function():
+    if True:
+        raise ImageNotFound(
+            f"The image is not found!"
+        )
+
+def test_ImageNotFound_is_raised():
+    """
+    GIVEN an ImageNotFound exception
+    WHEN fake_ImageNotFound_function is called
+    THEN it should raise ImageNotFound
+    """
+    with pytest.raises(ImageNotFound):
+        fake_ImageNotFound_function()
