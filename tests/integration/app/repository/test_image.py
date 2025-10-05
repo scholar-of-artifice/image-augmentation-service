9be195342026963ba096c0028cfe41b541899d7a
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-10-05 12:32:30 +0100

    add - test_read_UnprocessedImage_entry_success

--- tests/integration/app/repository/test_image.py
+++ tests/integration/app/repository/test_image.py
@@ -5,7 +5,7 @@ import pytest
 import sqlalchemy
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.repository.image import create_UnprocessedImage_entry
+from app.repository.image import create_UnprocessedImage_entry, read_UnprocessedImage_entry
 from app.schemas.transactions_db import UnprocessedImage, User
 
 pytestmark = pytest.mark.asyncio
@@ -52,3 +52,30 @@ async def test_create_UnprocessedImage_entry_success(
     assert db_entry is not None
     assert db_entry.original_filename == test_original_filename
     assert db_entry.user_id == test_user_id
+
+
+async def test_read_UnprocessedImage_entry_success(
+        async_db_session: AsyncSession,
+        test_user: User,
+):
+    # create fake test data
+    test_original_filename = 'my_cool_image.png'
+    test_storage_filename = f"{uuid.uuid4()}.png"
+    fake_user = await test_user
+    test_user_id = fake_user.id
+    # create an UnprocessedImage
+    new_image_entry = await create_UnprocessedImage_entry(
+        original_filename=test_original_filename,
+        storage_filename=test_storage_filename,
+        user_id=test_user_id,
+        db_session=async_db_session,
+    )
+    # call the function
+    read_entry = await read_UnprocessedImage_entry(
+        image_id=new_image_entry.id,
+        user_id=test_user_id,
+        db_session=async_db_session,
+    )
+    # check the results
+    assert isinstance(read_entry, UnprocessedImage)
+    assert read_entry == new_image_entry
