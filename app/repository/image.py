279a1a8b4e298144d39e1f053208de7e343309d3
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-10-05 11:21:43 +0100

    add - create_unprocessed_image_entry

--- app/repository/image.py
+++ app/repository/image.py
@@ -9,3 +9,21 @@ from app.db.database import get_async_session
 async def store_unprocessed_image(
 ) -> None:
     return None
+
+
+async def create_unprocessed_image_entry(
+    original_filename: str,
+    storage_filename: str,
+    user_id: uuid.UUID,
+    db_session: AsyncSession = Depends(get_async_session)
+) -> UnprocessedImage:
+    new_entry = UnprocessedImage(
+        original_filename=original_filename,
+        storage_filename=storage_filename,
+        user_id=user_id,
+    )
+    db_session.add(new_entry)
+    await db_session.flush()
+    await db_session.refresh(new_entry)
+    await db_session.commit()
+    return new_entry
