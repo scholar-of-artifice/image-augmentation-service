1d143551ad4abcdf635784aa07f7293e223d72e8
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-10-06 22:22:23 +0100

    add - test_write_unprocessed_image_success

--- tests/unit/app/repository/test_directory_manager.py
+++ tests/unit/app/repository/test_directory_manager.py
@@ -1,12 +1,14 @@
 import uuid
+from unittest.mock import MagicMock
 
 import pytest
 from pathlib import Path
 from app.repository.directory_manager import (
 VOLUME_PATHS,
-create_unprocessed_user_directory
+create_unprocessed_user_directory,
+write_unprocessed_image
 )
-
+import numpy
 pytestmark = pytest.mark.asyncio
 
 async def test_VOLUME_PATHS_has_correct_structure():
@@ -34,3 +36,36 @@ async def test_create_unprocessed_user_directory_creates_new_directory(mocker):
     new_path = await create_unprocessed_user_directory(user_id=fake_user_id)
     # do checks
     assert new_path == expected_path
+
+async def test_write_unprocessed_image_success(mocker):
+    """
+    GIVEN an image
+    AND a user_id
+    AND a storage_filename
+    WHEN write_unprocessed_image is called
+    THEN it should create the new directory
+    """
+    # make fake image data
+    fake_image_data = numpy.random.random((4, 4, 3))
+    fake_user_id = uuid.uuid4()
+    fake_storage_filename = f"{uuid.uuid4()}.png"
+    #
+    mock_image_instance = MagicMock()
+    mock_fromarray = mocker.patch(
+        "app.repository.directory_manager.Image.fromarray",
+        return_value=mock_image_instance
+    )
+    # call the funciton
+    result_path = await write_unprocessed_image(
+        image_data=fake_image_data,
+        user_id=fake_user_id,
+        storage_filename=fake_storage_filename,
+    )
+    expected_path = VOLUME_PATHS["unprocessed_image_data"] / str(fake_user_id) / fake_storage_filename
+    mock_fromarray.assert_called_once_with(obj= fake_image_data, mode= "RGB")
+    mock_image_instance.save.assert_called_once_with(
+        fp=expected_path,
+        format='PNG'
+    )
+    assert result_path == expected_path
+
