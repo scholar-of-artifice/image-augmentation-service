b749a0fb05f59a509fc6606458094bd3d8c25849
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-10-06 21:28:38 +0100

    add - test_create_unprocessed_user_directory_creates_new_directory

--- tests/unit/app/repository/test_directory_manager.py
+++ tests/unit/app/repository/test_directory_manager.py
@@ -1,7 +1,10 @@
+import uuid
+
 import pytest
 from pathlib import Path
 from app.repository.directory_manager import (
-VOLUME_PATHS
+VOLUME_PATHS,
+create_unprocessed_user_directory
 )
 
 pytestmark = pytest.mark.asyncio
@@ -16,3 +19,18 @@ async def test_VOLUME_PATHS_has_correct_structure():
         "unprocessed_image_data": Path('/image-augmentation-service/data/images/unprocessed'),
         "processed_image_data": Path('/image-augmentation-service/data/images/processed'),
     }
+
+async def test_create_unprocessed_user_directory_creates_new_directory(mocker):
+    """
+    GIVEN a subdirectory for a user does not exist
+    WHEN create_unprocessed_user_directory is called
+    THEN it should create the new directory
+    """
+    fake_user_id = uuid.uuid4()
+    expected_path = VOLUME_PATHS["unprocessed_image_data"] / str(fake_user_id)
+    # mock the file system
+    mock_mkdir = mocker.patch("pathlib.Path.mkdir")
+    # call the function
+    new_path = await create_unprocessed_user_directory(user_id=fake_user_id)
+    # do checks
+    assert new_path == expected_path
