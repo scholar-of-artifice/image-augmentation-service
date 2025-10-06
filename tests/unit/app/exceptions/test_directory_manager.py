46dcc37ff76694f277279f9205ecfd25510dc395
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-10-06 08:38:03 +0100

    add - test_ImageDirectoryNotFound_is_raised

--- tests/unit/app/exceptions/test_directory_manager.py
+++ tests/unit/app/exceptions/test_directory_manager.py
@@ -1,11 +1,21 @@
 import pytest
 
-from app.exceptions.directory_manager import UserDirectoryNotFound
+from app.exceptions.directory_manager import (
+    ImageDirectoryNotFound,
+    UserDirectoryNotFound,
+)
 
 
 def fake_UserDirectoryNotFound_function():
     if True:
         raise UserDirectoryNotFound(
+            "The user directory is not found!"
+        )
+
+
+def fake_ImageDirectoryNotFound_function():
+    if True:
+        raise ImageDirectoryNotFound(
             "The user directory is not found!"
         )
 
@@ -17,3 +27,13 @@ def test_UserDirectoryNotFound_is_raised():
     """
     with pytest.raises(UserDirectoryNotFound):
         fake_UserDirectoryNotFound_function()
+
+
+def test_ImageDirectoryNotFound_is_raised():
+    """
+    GIVEN an ImageDirectoryNotFound exception
+    WHEN fake_ImageDirectoryNotFound_function is called
+    THEN it should raise ImageDirectoryNotFound
+    """
+    with pytest.raises(ImageDirectoryNotFound):
+        fake_ImageDirectoryNotFound_function()
