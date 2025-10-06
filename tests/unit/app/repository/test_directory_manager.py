8308515a42ad7b6309fe6886bc23752649ef80eb
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-10-06 08:58:24 +0100

    edit - add docstring

--- tests/unit/app/repository/test_directory_manager.py
+++ tests/unit/app/repository/test_directory_manager.py
@@ -5,6 +5,11 @@ VOLUME_PATHS
 )
 
 def test_VOLUME_PATHS_has_correct_structure():
+    """
+    GIVEN a VOLUME_PATHS
+    WHEN the VOLUME_PATHS structure is read
+    THEN it should have the correct structure
+    """
     assert VOLUME_PATHS == {
         "unprocessed_image_data": Path('/image-augmentation-service/data/images/unprocessed'),
         "processed_image_data": Path('/image-augmentation-service/data/images/processed'),
