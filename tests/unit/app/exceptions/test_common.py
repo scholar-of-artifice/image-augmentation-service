4c3716de8382338173af7c710e70d7b413e86141
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-10-06 08:32:11 +0100

    add - test_PermissionDenied_is_raised

---
+++ tests/unit/app/exceptions/test_common.py
@@ -0,0 +1,19 @@
+import pytest
+
+from app.exceptions import PermissionDenied
+
+
+def fake_PermissionDenied_function():
+    if True:
+        raise PermissionDenied(
+            "You do not have permission to do that!"
+        )
+
+def test_PermissionDenied_is_raised():
+    """
+    GIVEN a PermissionDenied exception
+    WHEN fake_PermissionDenied_function is called
+    THEN it should raise PermissionDenied
+    """
+    with pytest.raises(PermissionDenied):
+        fake_PermissionDenied_function()
