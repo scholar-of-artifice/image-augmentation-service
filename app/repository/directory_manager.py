9d115fa9705db83c5678f56ca0ad4666210a84b8
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-10-06 08:45:47 +0100

    edit - uv run ruff check --fix app/repository/directory_manager.py

---
+++ app/repository/directory_manager.py
@@ -0,0 +1,78 @@
+"""
+This module contains a number of functions for creating, reading and deleting directories.
+"""
+import uuid
+
+
+async def create_unprocessed_user_directory(
+        user_id: uuid.UUID,
+) -> None:
+    """
+    Create an unprocessed image directory.
+    """
+    # TODO: check if subdirectory exists
+    # /image-augmentation-service/data/images/unprocessed/{user_id}/
+    # TODO: create subdirectory
+    return None
+
+
+async def delete_unprocessed_user_directory(
+        user_id: uuid.UUID,
+) -> None:
+    """
+    Delete an unprocessed image directory.
+    """
+    # TODO: check if subdirectory exists
+    # /image-augmentation-service/data/images/unprocessed/{user_id}/
+    # TODO: delete subdirectory and all internal contents
+    return None
+
+
+async def create_processed_user_directory(
+        user_id: uuid.UUID,
+) -> None:
+    """
+    Create a processed image directory.
+    """
+    # TODO: check if subdirectory exists
+    # /image-augmentation-service/data/images/processed/{user_id}/
+    # TODO: create subdirectory
+    return None
+
+
+async def delete_processed_user_directory(
+        user_id: uuid.UUID,
+) -> None:
+    """
+    Delete a processed image directory.
+    """
+    # TODO: check if subdirectory exists
+    # /image-augmentation-service/data/images/processed/{user_id}/
+    # TODO: delete subdirectory and all internal contents
+    return None
+
+
+async def create_processed_image_directory(
+        user_id: uuid.UUID,
+        image_id: uuid.UUID,
+) -> None:
+    """
+    Create a processed image directory.
+    """
+    # TODO: check if subdirectory exists
+    # /image-augmentation-service/data/images/processed/{user_id}/{image_id}
+    # TODO: create subdirectory
+    return None
+
+
+async def delete_processed_image_directory(
+        user_id: uuid.UUID,
+        image_id: uuid.UUID,
+) -> None:
+    """
+    Delete a processed image directory.
+    """
+    # TODO: check if subdirectory exists
+    # /image-augmentation-service/data/images/processed/{user_id}/{image_id}
+    # TODO: delete subdirectory
+    return None
