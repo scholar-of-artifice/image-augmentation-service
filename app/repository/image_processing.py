b0722822831e5c2aaec7dc4cc90d1806b36ce96c
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-10-20 09:55:24 +0100

    edit - add mute_channel to function map

--- app/repository/image_processing.py
+++ app/repository/image_processing.py
@@ -36,6 +36,7 @@ PROCESSING_MAP = {
     'invert': invert,
     'shift': shift,
     'rotate': rotate,
+    'mute_channel': mute_channel,
     'percentile_filter': percentile_filter,
     'rainbow_noise': rainbow_noise,
     'salt_noise': salt_noise,
