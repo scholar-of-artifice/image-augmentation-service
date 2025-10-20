647b734649d465c2710f0158c17a95186b57b8eb
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-10-20 09:37:19 +0100

    edit - reorder function map

--- app/repository/image_processing.py
+++ app/repository/image_processing.py
@@ -16,14 +16,14 @@ from app.schemas.image import AugmentationRequestBody
 # map a string in the input parameter to an augmentation function
 PROCESSING_MAP = {
     'brighten': brighten,
+    'channel_swap': channel_swap,
+    'cutout': cutout,
     'shift': shift,
     'rotate': rotate,
     'rainbow_noise': rainbow_noise,
     'salt_noise': salt_noise,
     'pepper_noise': pepper_noise,
     'flip': flip,
-    'channel_swap': channel_swap,
-    'cutout': cutout,
 }
 # TODO: add more functions
 # flip
