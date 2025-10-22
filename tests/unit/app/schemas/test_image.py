82448849c82da0d8e639057d9dfcbe84b6dae216
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-10-22 19:26:27 +0100

    add - test_MinFilterArguments_with_amount_value_of_1_is_valid

--- tests/unit/app/schemas/test_image.py
+++ tests/unit/app/schemas/test_image.py
@@ -297,6 +297,16 @@ def test_MaxFilterArguments_with_amount_value_of_100_is_valid():
 
 
 # --- MinFilterArguments ---
+
+def test_MinFilterArguments_with_amount_value_of_1_is_valid():
+    data = {
+        "processing": "min_filter",
+        "amount": 1
+    }
+    min_filter_args = MinFilterArguments(**data)
+    min_filter_args.processing = "min_filter"
+    min_filter_args.amount = 1
+
 # --- MuteChannelArguments ---
 # --- PepperNoiseArguments ---
 # --- PercentileFilterArguments ---
