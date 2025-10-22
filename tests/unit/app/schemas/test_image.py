643f294da3bb7c5861a65b717c20bde0a912e907
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-10-22 19:42:09 +0100

    add - test_PepperNoiseArguments_with_amount_value_of_100_is_valid

--- tests/unit/app/schemas/test_image.py
+++ tests/unit/app/schemas/test_image.py
@@ -356,6 +356,15 @@ def test_PepperNoiseArguments_with_amount_value_of_1_is_valid():
     mute_channel_args.processing = "pepper_noise"
     mute_channel_args.amount = 1
 
+def test_PepperNoiseArguments_with_amount_value_of_100_is_valid():
+    data = {
+        "processing": "pepper_noise",
+        "amount": 100
+    }
+    mute_channel_args = PepperNoiseArguments(**data)
+    mute_channel_args.processing = "pepper_noise"
+    mute_channel_args.amount = 100
+
 # --- PercentileFilterArguments ---
 # --- RainbowNoiseArguments ---
 
